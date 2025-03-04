3bf6af726481f6147b6d4846c098cac9b03fff9d
Pau <pausoltor@gmail.com>
2025-03-04 20:29:19 +0100

    Add search for player

--- Fast-and-fractured/Assets/EnemyAI/Scripts/EnemyAIBrain.cs
+++ Fast-and-fractured/Assets/EnemyAI/Scripts/EnemyAIBrain.cs
@@ -9,10 +9,17 @@ public class EnemyAIBrain : MonoBehaviour
     [SerializeField] NavMeshAgent agent;
 
     private Vector3 _positionToDrive;
+    private GameObject _player;
     public Vector3 PositionToDrive { get => _positionToDrive; set => _positionToDrive = value; }
+    public GameObject Player { get => _player; set => _player = value; }
 
     public void GoToPosition()
     {
         agent.SetDestination(_positionToDrive);
+    }
+
+    public void SearchPlayer()
+    {
+        _positionToDrive = _player.transform.position;
     }
 }
