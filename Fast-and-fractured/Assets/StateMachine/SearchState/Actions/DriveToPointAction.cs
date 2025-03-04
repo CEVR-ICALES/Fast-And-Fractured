4efeedfc476ff55de77bec042617e2ebe58057e3
Pau <pau.sole@gracia.lasalle.cat>
2025-03-04 20:17:53 +0100

    Create drive to point action

---
+++ Fast-and-fractured/Assets/StateMachine/SearchState/Actions/DriveToPointAction.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using StateMachine;
+
+[CreateAssetMenu(fileName = "DriveToPoint", menuName = "EnemyStateMachine/Actions/DriveToPoint")]
+
+public class DriveToPointAction : Action
+{
+    public override void Act(Controller controller)
+    {
+        EnemyAIBrain brain = controller.GetComponentInController<EnemyAIBrain>();
+
+        brain.GoToPosition();
+    }
+}
