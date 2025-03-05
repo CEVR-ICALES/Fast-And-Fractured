aae2e0725d8cec848f5d9537405b2a74bf53d2a7
Álvaro Miguel Bárcena <alvaro207mb@gmail.com>
2025-03-05 18:59:35 +0100

    working speed limiters, speed overlay

--- Fast-and-fractured/Assets/Player/CarMovement/Scripts/CarMovementController.cs
+++ Fast-and-fractured/Assets/Player/CarMovement/Scripts/CarMovementController.cs
@@ -1,10 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class CarMovementController : MonoBehaviour
 {
     public WheelController[] wheels;
+    public TextMeshProUGUI speedOverlay;
+    public bool applyRollPrevention = true;
+    [Tooltip("Value on km/h, in code has to be / 3.6 to be converted to m/s")]
+    public float maxRbVelocity;
 
     private PlayerInputController _playerInputController;
     private RollPrevention _rollPrevention;
@@ -15,7 +20,6 @@ public class CarMovementController : MonoBehaviour
     [SerializeField] private float motorTorque;
     [SerializeField] private float maxSteerAngle;
     [SerializeField] private float steeringSmoothness;
-
 
     [Header("Brake Settings")]
     public BRAKE_MODE brakeMode = BRAKE_MODE.AllWheels;
@@ -31,6 +35,7 @@ public class CarMovementController : MonoBehaviour
     private float targetSteerAngle;
     private float currentSteerAngle;
     private Vector2 steeringInput;
+    private float currentRbMaxVelocity;
 
 
     private void Start()
@@ -38,6 +43,7 @@ public class CarMovementController : MonoBehaviour
         _playerInputController = GetComponent<PlayerInputController>();
         _rollPrevention = GetComponent<RollPrevention>();
         _carRb = GetComponent<Rigidbody>();
+        currentRbMaxVelocity = maxRbVelocity;
     }
     private void OnEnable()
     {
@@ -51,10 +57,14 @@ public class CarMovementController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        float steeringInputMagnitude = steeringInput.magnitude;
-        _rollPrevention.ApplyRollPrevention(_carRb, steeringInputMagnitude);
-
+        if (applyRollPrevention)
+        {
+            float steeringInputMagnitude = steeringInput.y;
+            _rollPrevention.ApplyRollPrevention(_carRb, steeringInputMagnitude);
+        }
+       
         UpdateWheelVisuals();
+        LimitRigidBodySpeed();
 
     }
 
@@ -62,11 +72,20 @@ public class CarMovementController : MonoBehaviour
     {
         HandleInput();
         ApplySteering();
+        UpdateSpeedOverlay();
     }
 
     private void HandleInput()
     {
         steeringInput = _playerInputController.moveInput;
+        
+        if(steeringInput.y < 0.05f)//should create a small threshold to consider if the button is being clicked or not
+        {
+            foreach (var wheel in wheels)
+            {
+                wheel.ApplyMotorTorque(0f);
+            }
+        }
 
         float acceleration = steeringInput.y * motorTorque;
         foreach(var wheel in wheels)
@@ -114,9 +133,20 @@ public class CarMovementController : MonoBehaviour
             case BRAKE_MODE.FrontWheelsStronger:
                 wheels[0].ApplyBrakeTorque(brakeTorque * 0.8f);
                 wheels[1].ApplyBrakeTorque(brakeTorque * 0.8f);
-                wheels[2].ApplyBrakeTorque(brakeTorque * 0.2f);
-                wheels[3].ApplyBrakeTorque(brakeTorque * 0.2f);
+                //wheels[2].ApplyBrakeTorque(brakeTorque * 0.2f);
+                //wheels[3].ApplyBrakeTorque(brakeTorque * 0.2f);
                 break;
+        }
+    }
+
+    private void LimitRigidBodySpeed()
+    {
+        Vector3 clampedVelocity = _carRb.velocity;//get current speed
+
+        if (clampedVelocity.magnitude > (currentRbMaxVelocity / 3.6f))
+        {
+            clampedVelocity = clampedVelocity.normalized * (currentRbMaxVelocity / 3.6f);//retain direction (normalize) & scale it down to then apply it to rbvelocirty
+            _carRb.velocity = clampedVelocity;
         }
     }
 
@@ -177,18 +207,20 @@ public class CarMovementController : MonoBehaviour
         {
             _isDashing = true;
             Vector3 dashDirection = transform.forward.normalized;
+            currentRbMaxVelocity = maxRbVelocityWhileDashing;
             TimerManager.Instance.StartTimer(1.5f, () =>
             {
                 _isDashing = false;
+                currentRbMaxVelocity = maxRbVelocity;
             }, (progress) => {
                 _carRb.AddForce(dashDirection * dashForce, ForceMode.Impulse);
-                Vector3 clampedVelocity = _carRb.velocity;//get current speed
+                /*Vector3 clampedVelocity = _carRb.velocity;//get current speed
 
                 if (clampedVelocity.magnitude > maxRbVelocityWhileDashing)
                 {
                     clampedVelocity = clampedVelocity.normalized * maxRbVelocityWhileDashing;//retain direction (normalize) & scale it down to then apply it to rbvelocirty
                     _carRb.velocity = clampedVelocity;
-                }
+                }*/
 
             }, "dash", false, false);
         }
@@ -200,5 +232,12 @@ public class CarMovementController : MonoBehaviour
         {
             wheel.UpdateWheelVisuals();
         }
+    }
+
+    private void UpdateSpeedOverlay()
+    {
+        float speedZ = Mathf.Abs(_carRb.velocity.z);
+        float speedKmh = speedZ * 3.6f;
+        speedOverlay.text = "Speed: " + speedKmh.ToString("F1") + " km/h";
     }
 }
