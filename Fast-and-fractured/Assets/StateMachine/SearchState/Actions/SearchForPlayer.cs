67789db49eb4829b95ca3d64bfbbd82e30dd7993
Pau <pausoltor@gmail.com>
2025-03-05 19:15:50 +0100

    Create search player action

--- Fast-and-fractured/Assets/StateMachine/SearchState/Actions/SearchForPlayer.cs
+++ Fast-and-fractured/Assets/StateMachine/SearchState/Actions/SearchForPlayer.cs
@@ -3,6 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "SearchPlayer", menuName = "EnemyStateMachine/Actions/SearchPlayer")]
+
 public class SearchForPlayer : Action
 {
     public override void Act(Controller controller)
