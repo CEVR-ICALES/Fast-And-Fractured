4558a67698e3228e8e849b141068e08a2e5a28dd
CortesJoan <cortesjoancontact@gmail.com>
2025-03-06 19:32:19 +0100

    Added dummy methods in EnemyAIBrain

--- Fast-and-fractured/Assets/FastAndFractured/EnemyAI/Scripts/EnemyAIBrain.cs
+++ Fast-and-fractured/Assets/FastAndFractured/EnemyAI/Scripts/EnemyAIBrain.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,6 +8,7 @@ public class EnemyAIBrain : MonoBehaviour
 {
     //Setted in inspector
     [SerializeField] NavMeshAgent agent;
+    [SerializeField] float fleeMagnitude = 5;
 
     private Vector3 _positionToDrive;
     private GameObject _player;
@@ -31,8 +33,9 @@ public class EnemyAIBrain : MonoBehaviour
         agent.SetDestination(_positionToDrive);
     }
 
-    public void SearchPlayer()
+    public void SearchPlayerPosition()
     {
+        AssignTarget(_player);
         _positionToDrive = _player.transform.position;
     }
 
@@ -59,5 +62,26 @@ public class EnemyAIBrain : MonoBehaviour
         //TODO
         //Use unique ability
     }
+    public bool IsDashAvailable()
+    {
+        return false;
+    }
 
+    public void RunAwayFromCurrentTarget()
+    {
+       Vector3 targetDirection = (transform.position - _targetToShoot.transform.position).normalized;
+
+        _positionToDrive = targetDirection * fleeMagnitude;
+    }
+
+
+    private void AssignTarget(GameObject target)
+    {
+        _targetToShoot = target;
+    }
+
+    internal bool IsDashFinished()
+    {
+        return false;
+    }
 }
