5086eb15a33fcaf9f005f02fd438c43dffac971b
Álvaro Miguel Bárcena <alvaro207mb@gmail.com>
2025-03-10 20:26:30 +0100

    started car physics interactions, pushing etc

--- Fast-and-fractured/Assets/FastAndFractured/Player/CarMovement/Scripts/PhysicsBehaviour.cs
+++ Fast-and-fractured/Assets/FastAndFractured/Player/CarMovement/Scripts/PhysicsBehaviour.cs
@@ -4,9 +4,25 @@ using UnityEngine;
 
 public class PhysicsBehaviour : MonoBehaviour
 {
+    [Header("Provisional Values")] //values that are not going to be on this script (player stats / car stats)
+    [SerializeField] private float _frontalHitAnlgeThreshold; // angle to detect whether the dash hit was frontal or not
+    public float _maxEndurance { get; private set; }
+    public float _endurance { get; private set; }
+    
+    public float weight { get; private set; }
     [SerializeField] private float _baseForce;
 
     private CarMovementController _carMovementController;
+    private Rigidbody _rb;
+
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody>();   
+        _carMovementController = GetComponent<CarMovementController>();
+        weight = _rb.mass;
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         // add on trigger enter logic
@@ -14,13 +30,48 @@ public class PhysicsBehaviour : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        //collision.
+        PhysicsBehaviour otherComponenPhysicsBehaviours;
+        if(collision.gameObject.TryGetComponent(out otherComponenPhysicsBehaviours))
+        {
+            ContactPoint contactPoint = collision.contacts[0];
+            Vector3 collisionPos = contactPoint.point;
+            Vector3 ownDirection = transform.forward;
+            float otherCarEnduranceFactor = otherComponenPhysicsBehaviours._endurance / otherComponenPhysicsBehaviours._maxEndurance; // calculate current value of the other car endurance
+            float otherCarWeight = otherComponenPhysicsBehaviours.weight;
+            //detect if the contact was frontal
+            if (Vector3.Angle(transform.forward, -collision.gameObject.transform.forward) <= _frontalHitAnlgeThreshold)
+            {
+                // logic to determine forc 
+            } else
+            {
+
+            }
+        }
+        
+       
+
     }
 
-    /*public void ApplyForce(float )
+    public void ApplyForce(Vector3 forceDirection, Vector3 forcePoint, float forceToApply)
+    {
+        _rb.AddForceAtPosition(forceDirection * forceToApply, forcePoint, ForceMode.Impulse);
+    }
+
+
+    private void CalculateForceToApplyToOtherCar(float oCarEnduranceFactor, float oCarWeight)
     {
 
-    }*/
+    }
+
+    public void BlockRigidBodyRotations()
+    {
+
+    }
+
+    public void UnblockRigidBodyRotations()
+    {
+
+    }
 
 
     
