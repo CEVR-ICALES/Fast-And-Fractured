d7eb6629d622225e4d245ff272bc2349500c5a1d
CortesJoan <73123760+CortesJoan@users.noreply.github.com>
2025-03-11 12:20:58 +0100

    Created universal ANDDecision to evaluate multiple decisions

---
+++ Fast-and-fractured/Assets/StateMachine/Decisions/ANDDecision.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using StateMachine;
+
+public class ANDDecision : Decision
+{
+    [SerializeField] Decision[] decisions;
+    public override bool Decide(Controller controller)
+    {
+        bool success = true;
+        for (int i = 0; i < decisions.Length && success; i++)
+        {
+            success = decisions[i].Decide(controller);
+        }
+        return success;
+    }
+
+}
