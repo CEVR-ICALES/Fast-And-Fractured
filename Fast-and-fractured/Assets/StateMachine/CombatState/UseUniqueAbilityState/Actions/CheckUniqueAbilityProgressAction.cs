aafee748301b8cf47064ec69a5c8e6f5ace6bfa3
CortesJoan <73123760+CortesJoan@users.noreply.github.com>
2025-03-11 14:32:46 +0100

    Marked as obsolete CheckUniqueAvilityPorgressAction

--- Fast-and-fractured/Assets/StateMachine/CombatState/UseUniqueAbilityState/Actions/CheckUniqueAbilityProgressAction.cs
+++ Fast-and-fractured/Assets/StateMachine/CombatState/UseUniqueAbilityState/Actions/CheckUniqueAbilityProgressAction.cs
@@ -1,8 +1,12 @@
+using System;
 using StateMachine;
 using System.Collections;
 using System.Collections.Generic;
+using Microsoft.Unity.VisualStudio.Editor;
 using UnityEngine;
+using Action = StateMachine.Action;
 
+[Obsolete("This action is not used anymore use   IsUniqueAbilityFinishedDecision instead ")]
 public class CheckUniqueAbilityProgressAction : Action
 {
     public override void Act(Controller controller)
