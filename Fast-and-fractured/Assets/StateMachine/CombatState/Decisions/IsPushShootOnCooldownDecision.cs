ce017571c5921625ac3d017ee29fc6ea733ea5be
CortesJoan <cortesjoancontact@gmail.com>
2025-03-12 17:34:02 +0100

    Renamed some decisions to follow the guidelines more closely

--- Fast-and-fractured/Assets/StateMachine/CombatState/Decisions/IsPushShootOnCooldownDecision.cs
+++ Fast-and-fractured/Assets/StateMachine/CombatState/Decisions/IsPushShootOnCooldownDecision.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using StateMachine;
 
-[CreateAssetMenu(fileName = nameof(IsShootOverheated), menuName = "EnemyStateMachine/Decisions/IsShootOverheated")]
-public class IsShootOverheated : Decision
+[CreateAssetMenu(fileName = nameof(IsPushShootOnCooldownDecision), menuName = "EnemyStateMachine/Decisions/IsPushShootOnCooldownDecision")]
+public class IsPushShootOnCooldownDecision : Decision
 {
     public override bool Decide(Controller controller)
     {
         EnemyAIBrain brain = controller.GetBehaviour<EnemyAIBrain>();
 
-        return brain.IsShootOverheated();
+        return brain.IsPushShootReady();
     }
 }
--- Fast-and-fractured/Assets/StateMachine/CombatState/Decisions/IsShootOverheatedDecision.cs
+++ Fast-and-fractured/Assets/StateMachine/CombatState/Decisions/IsShootOverheatedDecision.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using StateMachine;
 
-[CreateAssetMenu(fileName = nameof(IsPushShootOnCooldown), menuName = "EnemyStateMachine/Decisions/IsPushShootOnCooldown")]
-public class IsPushShootOnCooldown : Decision
+[CreateAssetMenu(fileName = nameof(IsShootOverheatedDecision), menuName = "EnemyStateMachine/Decisions/IsShootOverheatedDecision")]
+public class IsShootOverheatedDecision : Decision
 {
     public override bool Decide(Controller controller)
     {
         EnemyAIBrain brain = controller.GetBehaviour<EnemyAIBrain>();
 
-        return brain.IsPushShootReady();
+        return brain.IsShootOverheated();
     }
 }
