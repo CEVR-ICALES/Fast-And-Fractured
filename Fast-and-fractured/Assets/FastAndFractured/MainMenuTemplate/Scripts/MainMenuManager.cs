3e4b7e7f0d61db302c2becd4e001f9ab5a2eca17
Adrián Ontiveros Cruz <adrionticruz@gmail.com>
2025-03-21 15:42:25 +0100

    main menu basic scripts done and implemented

--- Fast-and-fractured/Assets/FastAndFractured/MainMenuTemplate/Scripts/MainMenuManager.cs
+++ Fast-and-fractured/Assets/FastAndFractured/MainMenuTemplate/Scripts/MainMenuManager.cs
@@ -1,18 +1,96 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
+public enum ScreensType
+{
+    MAIN_MENU,
+    SETTINGS,
+    CREDITS,
+    GAMEMODE_SELECTION,
+    CHARACTER_SELECTION,
+    LOADING,
+    SPLASH_SCREEN
+} 
 public class MainMenuManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    #region Singleton
+
+    public static MainMenuManager Instance { get; private set; }
+
+    #endregion
+
+    #region Private Fields
+
+    private Dictionary<ScreensType, MenuScreen> _menuScreens = new Dictionary<ScreensType, MenuScreen>();
+    private MenuScreen _currentScreen;
+
+    #endregion
+
+    #region Unity Methods
+
+    private void Awake()
     {
-        
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        // Register all Screens in scene
+        RegisterScreens();
+        _currentScreen = _menuScreens[ScreensType.MAIN_MENU];
     }
+
+    #endregion
+
+    #region Start Methods
+
+    void RegisterScreens()
+    {
+        foreach (MenuScreen screen in FindObjectsOfType<MenuScreen>(true))
+        {
+            _menuScreens[screen.screenType] = screen;
+        }
+    }
+
+    #endregion
+
+    #region Screen Management Methods
+
+    public void ChangeScreen(ScreensType screenType)
+    {
+        if (_currentScreen != null)
+        {
+            _currentScreen.gameObject.SetActive(false);
+        }
+        _currentScreen = _menuScreens[screenType];
+        _currentScreen.gameObject.SetActive(true);
+    }
+
+    #endregion
+
+    // THIS SHOULD BE ON A FUTURE GAME MANAGER OR CUSTOM SCENE MANAGER
+    #region Scene Management Methods
+
+    public void LoadScene(string sceneName)
+    {
+        if(SceneManager.GetSceneByName(sceneName).IsValid())
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+        else
+        {
+            Debug.LogWarning($"Scene '{sceneName}' not found");
+        }
+    }
+
+    #endregion
 }
