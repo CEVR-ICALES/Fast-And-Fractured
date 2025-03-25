f09b6c3ee317a7090ce288b9ee4ff16ec1679aef
CortesJoan <73123760+CortesJoan@users.noreply.github.com>
2025-03-25 14:51:48 +0100

    Created BaseIniqueAbility logic

---
+++ Fast-and-fractured/Assets/FastAndFractured/Characters/!Common/BaseUniqueAbility.cs
@@ -0,0 +1,121 @@
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Serialization;
+using Utilities;
+
+namespace Game
+{
+    public abstract class BaseUniqueAbility : MonoBehaviour
+    {
+        #region VARIABLES
+
+        [SerializeField] protected AbilityData abilityData;
+        private bool _isAbilityActive = false;
+        private bool _isAI;
+        private GameObject _uniqueUIPrefabInstance;
+        private GameObject _particleEffectPrefabInstance;
+         public UnityEvent onAbilityActivated;
+        public UnityEvent onAbilityEnded;
+        private bool _isOnCooldown = false; 
+        private float _currentCooldownTime = 0f;  
+        private ITimer _cooldownTimer;
+        #endregion
+        public bool IsAbilityActive => _isAbilityActive;
+        public bool IsOnCooldown => _isOnCooldown;
+
+        private void Awake()
+        {
+            if (abilityData == null)
+            {
+                Debug.LogError("Ability Data Is Null:  ", this);
+                return;
+            }
+
+            _isAbilityActive = false;
+            
+            _isAI = GetComponentInParent<EnemyAIBrain>();
+        }
+
+
+        public virtual void ActivateAbility()
+        {
+            if (_isAbilityActive || _isOnCooldown)
+            {
+                return;
+            }
+
+            _isAbilityActive = true;
+            _isOnCooldown = true;
+            PlayActivateAbilitySound();
+            StartAbilityEffects();
+            onAbilityActivated?.Invoke();
+            StartCooldown();
+        }  
+        private void StartCooldown()
+        {
+            _currentCooldownTime = abilityData.CooldownDuration;  
+            _cooldownTimer = TimerSystem.Instance.CreateTimer(abilityData.CooldownDuration, onTimerDecreaseComplete:StopCooldown) ;
+        }
+
+        protected virtual void PlayActivateAbilitySound()
+        {
+            if (abilityData == null || abilityData.FMODEventPath.IsNull)
+            {
+                return;
+            }
+            SoundManager.Instance.PlaySound3D(abilityData.FMODEventPath.Path, this.transform.position);
+        }
+
+        protected virtual void StartAbilityEffects()
+        {
+            GameObject uniqueUIPrefab = abilityData.UniqueUIPrefab;
+            if (uniqueUIPrefab != null && !_isAI)
+            {
+                _uniqueUIPrefabInstance = Instantiate(uniqueUIPrefab);
+            }
+
+            GameObject particleEffectPrefab = abilityData.ParticleEffectPrefab;
+
+            if (particleEffectPrefab != null)
+            {
+                _particleEffectPrefabInstance =
+                    Instantiate(particleEffectPrefab, this.transform.position, this.transform.rotation);
+            }
+        }
+
+
+        protected virtual void EndAbilityEffects()
+        {
+            DestroySpawnedGameObjectsInstances();
+            _isAbilityActive = false;
+            onAbilityEnded?.Invoke();
+        } 
+        
+        private void DestroySpawnedGameObjectsInstances()
+        {
+            Destroy(_particleEffectPrefabInstance);
+            Destroy(_uniqueUIPrefabInstance);
+        }
+       
+        public float GetNormalizedCooldownProgress()
+        {
+            if (_cooldownTimer== null)
+            {
+                return 1;
+            }
+            return _cooldownTimer.GetData().NormalizedProgress;
+        }
+ 
+        protected void StopCooldown()  
+        {
+            if (_cooldownTimer != null && TimerSystem.Instance.HasTimer(_cooldownTimer))
+            {
+                TimerSystem.Instance.StopTimer(_cooldownTimer.GetData().ID);  
+            }
+            _cooldownTimer = null; 
+            _isOnCooldown = false;
+            _currentCooldownTime = 0f;
+        }
+    }
+}
