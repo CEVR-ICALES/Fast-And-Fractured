25898a9bb9adc5adaf198cd9bebaf019aacf8b41
CortesJoan <73123760+CortesJoan@users.noreply.github.com>
2025-03-25 15:46:26 +0100

    Created IPausable interface  and basic Pausable implementations such as animation,physics and particles

---
+++ Fast-and-fractured/Assets/FastAndFractured/!Common/Managers/PauseSystem/AnimationPausable.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using Utilities.Managers.PauseSystem;
+
+public class AnimationPausable : MonoBehaviour, IPausable
+{
+    [SerializeField] private Animator _animator;
+    private float _originalSpeed;
+    void Start()
+    {
+        if (_animator == null)
+        {
+            _animator = GetComponentInChildren<Animator>();
+
+        }
+        PauseManager.Instance.RegisterPausable(this);
+    }
+
+    void OnDestroy()
+    {
+        PauseManager.Instance.UnregisterPausable(this);
+    }
+
+    public void OnPause()
+    {
+        if (_animator != null)
+        {
+            _originalSpeed = _animator.speed;
+            _animator.speed = 0f;  
+        }
+    }
+
+    public void OnResume()
+    {
+        if (_animator != null)
+        {
+            _animator.speed = _originalSpeed;  
+        }
+    }
+}
---
+++ Fast-and-fractured/Assets/FastAndFractured/!Common/Managers/PauseSystem/ParticleSystemPausable.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.Serialization;
+using Utilities.Managers.PauseSystem;
+
+public class ParticleSystemPausable : MonoBehaviour, IPausable
+{
+   [SerializeField] private ParticleSystem[] particleSystems;
+
+    void Start()
+    {
+        if (particleSystems.Length == 0)
+        {
+            particleSystems = GetComponentsInChildren<ParticleSystem>();
+        }
+
+        PauseManager.Instance.RegisterPausable(this);
+    }
+
+    void OnDestroy()
+    {
+        PauseManager.Instance.UnregisterPausable(this);
+    }
+
+    public void OnPause()
+    {
+        foreach (var ps in particleSystems)
+        {
+            ps.Pause();
+        }
+    }
+
+    public void OnResume()
+    {
+        foreach (var ps in particleSystems)
+        {
+            ps.Play();
+        }
+    }
+}
