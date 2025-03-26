06d5e418c350a605bf816479b89d34b19c84b795
Álvaro Miguel Bárcena <alvaro207mb@gmail.com>
2025-03-26 18:24:35 +0100

    created special ability scripts

---
+++ Fast-and-fractured/Assets/FastAndFractured/Characters/Carme/Scripts/CarmenUniqueAbilitie.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FastAndFractured
+{
+    public class CarmenUniqueAbilitie : BaseUniqueAbility
+    {
+        public GameObject chickenPrefab;
+
+        public override void ActivateAbility()
+        {
+            base.ActivateAbility();
+            
+
+
+        }
+
+        
+    }
+}
+
---
+++ Fast-and-fractured/Assets/FastAndFractured/Characters/Carme/Scripts/McChicken.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class McChicken : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        
+    }
+
+    //public void
+
+}
