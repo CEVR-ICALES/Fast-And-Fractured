c3a9d8cca0d5b2c818e64251d4914e5881808e09
Carlos Roig Garcia <carlos.roig.garcia9@gmail.com>
2025-03-26 22:45:56 +0100

    rollpreventionPreventetFromAirState

--- Fast-and-fractured/Assets/FastAndFractured/Player/StateMachines/PlayerStateMachine/AirState/Actions/DesactivateRollPreventionAction.cs
+++ Fast-and-fractured/Assets/FastAndFractured/Player/StateMachines/PlayerStateMachine/AirState/Actions/DesactivateRollPreventionAction.cs
@@ -1,10 +1,12 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using FastAndFractured;
-namespace StateMachine {
-    [CreateAssetMenu(fileName = "DesactiveRollPrevention", menuName = "PlayerStateMachine/Actions/ApplyRollPreventionAction")]
-    public class DesativateRollPreventionAction : Action
+using UnityEngine;
+namespace StateMachine
+{
+    [CreateAssetMenu(fileName = "DesactivateRollPreventionAction", menuName = "PlayerStateMachine/Actions/DesactivateRollPreventionAction")]
+    public class DesactivateRollPreventionAction : Action
     {
         public override void Act(Controller controller)
         {
