b6a2bf1e97c1975d1a8cfcd4fd2a745f9730f75e
RaulMR-12 <rmartinezromero12@gmail.com>
2025-03-27 19:34:25 +0100

    Added Super Maria Antonia Super Ultra Super Saiyan God of the death audio effect

---
+++ Fast-and-fractured/Assets/FastAndFractured/Characters/MariaAntonia/Scripts/MariaAntoniaUniqueAbility.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using FastAndFractured;
+
+public class MariaAntoniaUniqueAbility : AbilityData
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
