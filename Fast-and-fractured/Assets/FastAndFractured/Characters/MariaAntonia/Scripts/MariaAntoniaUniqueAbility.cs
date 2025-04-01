438f226e5067265619dd501c1e8c09c8f6f70e15
RaulMR-12 <rmartinezromero12@gmail.com>
2025-04-01 20:35:49 +0200

    Modfiied Script

--- Fast-and-fractured/Assets/FastAndFractured/Characters/MariaAntonia/Scripts/MariaAntoniaUniqueAbility.cs
+++ Fast-and-fractured/Assets/FastAndFractured/Characters/MariaAntonia/Scripts/MariaAntoniaUniqueAbility.cs
@@ -1,50 +1,59 @@
-using FastAndFractured;
 using FMODUnity;
-using UnityEngine;
 using Utilities;
+using UnityEngine;
+using System.Collections.Generic;
 
-public class MariaAntoniaUniqueAbility : BaseUniqueAbility
+namespace FastAndFractured
 {
-    #region Variables
-    [Tooltip("Multiplier to reduce the cooldown timers (example: 1.5 increases the cooldown timer by 50%")]
-    public float cooldownReductionMultiplier;
-
-    [Tooltip("Multiplier to increase the stats (example: 1.2 increases the stats by 20%")]
-    public float statBoostMultiplier;
-
-    [Tooltip("Unique ability duration in seconds")]
-    public float uniqueAbilityDuration;
-
-    public EventReference ssjUltiReference;
-
-    [SerializeField] private StatsController _statsController;
-    #endregion
-
-    public override void ActivateAbility()
+    public class MariaAntoniaUniqueAbility : BaseUniqueAbility
     {
-        if (IsAbilityActive || IsOnCooldown)
-            return;
+        #region Variables
+        [SerializeField] private StatsController _statsController;
 
-        base.ActivateAbility();
-        Debug.Log("Boosted Stats!");
+        [Tooltip("Multiplier to reduce the cooldown timers (example: 1.5 increases the cooldown timer by 50%")]
+        public float cooldownReductionMultiplier;
 
-        SoundManager.Instance.PlayOneShot(ssjUltiReference, transform.position);
+        [Tooltip("Multiplier to increase the stats (example: 1.2 increases the stats by 20%")]
+        public float statBoostMultiplier;
 
-        //_statsController = GetComponent<StatsController>();
+        [Tooltip("Unique ability duration in seconds")]
+        public float uniqueAbilityDuration;
 
-        if (_statsController == null)
+        public GameObject croquetasGameObject;
+
+        public int numberOfCroquetas;
+
+        private float _orbitRadius;
+
+        private float _orbitSpeed;
+
+        private List<GameObject> croquetaList = new List<GameObject> ();
+
+        public EventReference ssjUltiReference;
+
+        #endregion
+
+        public override void ActivateAbility()
         {
-            Debug.Log("Stats Controller not Found");
-            return;
+            if (IsAbilityActive || IsOnCooldown)
+                return;
+
+            base.ActivateAbility();
+
+            SoundManager.Instance.PlayOneShot(ssjUltiReference, transform.position);
+
+            if (_statsController == null)
+            {
+                Debug.Log("Stats Controller not Found");
+                return;
+            }
+
+            _statsController.TemporalProductStat(Enums.Stats.COOLDOWN_SPEED, cooldownReductionMultiplier, uniqueAbilityDuration);
+
+            _statsController.TemporalProductStat(Enums.Stats.MAX_SPEED, statBoostMultiplier, uniqueAbilityDuration);
+            _statsController.TemporalProductStat(Enums.Stats.ACCELERATION, statBoostMultiplier, uniqueAbilityDuration);
+            _statsController.TemporalProductStat(Enums.Stats.NORMAL_DAMAGE, statBoostMultiplier, uniqueAbilityDuration);
+            _statsController.TemporalProductStat(Enums.Stats.PUSH_DAMAGE, statBoostMultiplier, uniqueAbilityDuration);
         }
-
-        Debug.Log("After return");
-
-        _statsController.TemporalProductStat(Enums.Stats.COOLDOWN_SPEED, cooldownReductionMultiplier, uniqueAbilityDuration);
-
-        _statsController.TemporalProductStat(Enums.Stats.MAX_SPEED, statBoostMultiplier, uniqueAbilityDuration);
-        _statsController.TemporalProductStat(Enums.Stats.ACCELERATION, statBoostMultiplier, uniqueAbilityDuration);
-        _statsController.TemporalProductStat(Enums.Stats.NORMAL_DAMAGE, statBoostMultiplier, uniqueAbilityDuration);
-        _statsController.TemporalProductStat(Enums.Stats.PUSH_DAMAGE, statBoostMultiplier, uniqueAbilityDuration);
     }
 }
