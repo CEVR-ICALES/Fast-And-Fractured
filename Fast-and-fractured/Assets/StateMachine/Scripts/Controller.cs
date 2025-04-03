9f2033101c309e2397da1d088a057b8853eb84cb
CortesJoan <cortesjoancontact@gmail.com>
2025-04-03 20:17:32 +0200

    Now Controller can have added new behaviours in runtime

--- Fast-and-fractured/Assets/StateMachine/Scripts/Controller.cs
+++ Fast-and-fractured/Assets/StateMachine/Scripts/Controller.cs
@@ -166,5 +166,9 @@ namespace StateMachine
             PauseManager.Instance.UnregisterPausable(this);
         }
 
+      public void AddBehaviour(Behaviour monoBehaviour) {
+            availableBehaviours.Add(monoBehaviour);
+
+        }
     }
 }
