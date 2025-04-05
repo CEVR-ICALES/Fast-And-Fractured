af6b8bc9a0a6629e124b0ce32ca7bf3a28949cee
ImAlvaroMb <alvaro207mb@gmail.com>
2025-04-05 15:36:17 +0200

    trail movement vfx logic

--- Fast-and-fractured/Assets/FastAndFractured/Player/ParticleSystems/Scripts/VehicleVfxController.cs
+++ Fast-and-fractured/Assets/FastAndFractured/Player/ParticleSystems/Scripts/VehicleVfxController.cs
@@ -9,46 +9,85 @@ namespace FastAndFractured
         [Header("Car Movement Trail")]
         [SerializeField] private ParticleSystem[] trailParticleSystems;
         [SerializeField] private float movementThreshold;
+        [SerializeField] private float speedToEmitAllParticles;
+        private bool _carTrailParticlesActive = false;
+        private float _movementTrailInitialEmmisionRate = 200f; // shopuld always match the emissio nrate if its going to be modified
+        private ParticleSystem.EmissionModule[] _trailEmissionModules; // necessary to modify rateOverTime
 
 
         PhysicsBehaviour _physicsBehaviour;
+        CarMovementController _carMovementController;
+        private float _currentSpeed;
 
         private void Start()
         {
             _physicsBehaviour = GetComponent<PhysicsBehaviour>();
+            _carMovementController = GetComponent<CarMovementController>();
+
+            _trailEmissionModules = new ParticleSystem.EmissionModule[trailParticleSystems.Length];
+
+            for (int i = 0; i < trailParticleSystems.Length; i++)
+            {
+                _trailEmissionModules[i] = trailParticleSystems[i].emission;
+                
+            }
         }
 
         private void FixedUpdate()
         {
+            _currentSpeed = _physicsBehaviour.Rb.velocity.magnitude;
             HandleTrailParticleSystem();
         }
 
         private void HandleTrailParticleSystem()
         {
-            if(_physicsBehaviour.Rb.velocity.magnitude > movementThreshold)
+            if(!_carMovementController.IsGrounded())
             {
-                EnableParticles(trailParticleSystems);
+                StopParticles(trailParticleSystems, ref _carTrailParticlesActive);
+                return;
+            }
+
+            if(_currentSpeed > movementThreshold)
+            {
+                if(!_carTrailParticlesActive)
+                    EnableParticles(trailParticleSystems, ref _carTrailParticlesActive);
+                UpdateCarTrailMovementEmission();
             } else
             {
-                StopParticles(trailParticleSystems);
+                if(_carTrailParticlesActive)
+                    StopParticles(trailParticleSystems, ref _carTrailParticlesActive);
             }
         }
 
 
-        private void StopParticles(ParticleSystem[] particleSystems)
+        private void StopParticles(ParticleSystem[] particleSystems, ref bool boolToChange)
         {
             foreach(ParticleSystem particle in particleSystems)
             {
                 particle.Stop();
             }
+            boolToChange = false;
         }
 
-        private void EnableParticles(ParticleSystem[] particleSystems)
+        private void EnableParticles(ParticleSystem[] particleSystems, ref bool boolToChange)
         {
             foreach(ParticleSystem particle in particleSystems)
             {
                 particle.Play();
             }
+
+            boolToChange = true;
+        }
+
+        private void UpdateCarTrailMovementEmission()
+        {
+            float normalizedSpeed = Mathf.Clamp01(Mathf.InverseLerp(movementThreshold, speedToEmitAllParticles / 3.6f, _currentSpeed));
+            Debug.Log(normalizedSpeed);
+            for(int i = 0; i < _trailEmissionModules.Length; i++) // cant do foreach
+            {
+                _trailEmissionModules[i].rateOverTime = _movementTrailInitialEmmisionRate * normalizedSpeed;
+            }
+            
         }
     }
 }
