4545b01e24e163d36abe5f9477c26dee49eda2db
Pau <68166681+PauSolT@users.noreply.github.com>
2025-04-09 20:14:55 +0200

    add getter to AbilityData cooldownDuration

--- Fast-and-fractured/Assets/FastAndFractured/Characters/!Common/UniqueAbility/AbilityData/Scripts/AbilityData.cs
+++ Fast-and-fractured/Assets/FastAndFractured/Characters/!Common/UniqueAbility/AbilityData/Scripts/AbilityData.cs
@@ -6,16 +6,16 @@ namespace FastAndFractured
     [CreateAssetMenu(fileName = "NewAbilityData", menuName = "Abilities/Ability Data")]
     public class AbilityData : ScriptableObject
     {
-        [Header("Base Ability Settings")] 
+        [Header("Base Ability Settings")]
         [SerializeField] private string abilityName = "Unnamed Ability";
 
-        [SerializeField] [TextArea] private string abilityDescription = "Ability Description";
+        [SerializeField][TextArea] private string abilityDescription = "Ability Description";
         [SerializeField] private float duration = 5f;
         [SerializeField] private EventReference fMODEventPath;
         [SerializeField] private GameObject uniqueUIPrefab;
         [SerializeField] private GameObject particleEffectPrefab;
         [SerializeField] private string uniqueSentence = "Toma pa tus gastos!";
-        [Header("Cooldown Settings")]  
+        [Header("Cooldown Settings")]
         [SerializeField] private float cooldownDuration = 2f;
         public string AbilityName => abilityName;
         public string AbilityDescription => abilityDescription;
@@ -24,7 +24,7 @@ namespace FastAndFractured
         public GameObject UniqueUIPrefab => uniqueUIPrefab;
         public GameObject ParticleEffectPrefab => particleEffectPrefab;
         public string UniqueSentence => uniqueSentence;
-        
-        public float CooldownDuration => cooldownDuration;
+
+        public float CooldownDuration { get => cooldownDuration; set => cooldownDuration = value; }
     }
 }
