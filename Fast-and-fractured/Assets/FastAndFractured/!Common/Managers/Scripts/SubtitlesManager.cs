ae8cdeb490cfff32e291380a6a9da8278f8b2400
ImAlvaroMb <alvaro207mb@gmail.com>
2025-04-09 13:45:48 +0200

    subtitles working with lozalitzartion (provisiona subtitle manager))

---
+++ Fast-and-fractured/Assets/FastAndFractured/!Common/Managers/Scripts/SubtitlesManager.cs
@@ -0,0 +1,125 @@
+using Assets.SimpleLocalization.Scripts;
+using Enums;
+using TMPro;
+using UnityEngine;
+using Utilities;
+
+namespace FastAndFractured
+{
+    public struct SubtitleData
+    {
+        public string TextLocalizeCode;
+        public float Duration;
+        public Color TextColor;
+        public int FontSize;
+        public string SpeakerName;
+    }
+    public class SubtitlesManager : AbstractSingleton<SubtitlesManager>
+    {
+        [SerializeField] private TextMeshProUGUI subtitleText;
+        [SerializeField] private TextMeshProUGUI speakerText;
+        [SerializeField] private CanvasGroup canvasGroup;
+        [SerializeField] private float fadeDuration = 0.8f;
+
+        private bool _subtitlesEnabled;
+        private SubtitleData _currentSubtitle;
+        private ITimer _displayTimer;
+        private ITimer _fadeTimer;
+        private bool _isShowing = false;
+
+        private const string SUBTITLES_KEY = "Substitles";
+        protected override void Awake()
+        {
+            base.Awake();
+            ToggleSubtitles(PlayerPrefs.GetInt(SUBTITLES_KEY, 0) == 1);
+            canvasGroup.alpha = 0f;
+        }
+
+        public void ToggleSubtitles(bool enabled)
+        {
+            _subtitlesEnabled = enabled;
+            if (!_subtitlesEnabled)
+            {
+                HideImmediate();
+            }
+            else
+            {
+                canvasGroup.gameObject.SetActive(true);
+            }
+        }
+
+        public void ShowSubtitle(SubtitleData data)
+        {
+            if (!_subtitlesEnabled) return;
+
+            CleanupTimers();
+            _currentSubtitle = data;
+            _isShowing = true;
+            UpdateDisplay();
+            _fadeTimer = TimerSystem.Instance.CreateTimer(fadeDuration, TimerDirection.INCREASE,
+                onTimerIncreaseComplete: () => StartSubtitleDisplay(),
+                onTimerIncreaseUpdate: (progress) => canvasGroup.alpha = progress); ;
+        }
+
+        private void StartSubtitleDisplay()
+        {
+            // main display duration
+            canvasGroup.alpha = 1f;
+            _displayTimer = TimerSystem.Instance.CreateTimer(
+                _currentSubtitle.Duration,
+                onTimerDecreaseComplete: () => FadeOut()
+            );
+        }
+
+        private void FadeOut()
+        {
+            _fadeTimer = TimerSystem.Instance.CreateTimer(
+                fadeDuration,
+                TimerDirection.DECREASE,
+                onTimerDecreaseComplete: () => {
+                    _isShowing = false;
+                    canvasGroup.alpha = 0f;
+                },
+                onTimerDecreaseUpdate: (progress) => canvasGroup.alpha = progress
+            );
+        }
+
+        private void UpdateDisplay()
+        {
+            subtitleText.gameObject.GetComponent<LocalizedText>().LocalizationKey = _currentSubtitle.TextLocalizeCode;
+            subtitleText.gameObject.GetComponent<LocalizedText>().Localize();
+            //subtitleText.fontSize = _currentSubtitle.FontSize;
+            subtitleText.color = _currentSubtitle.TextColor;
+
+            if (speakerText != null)
+            {
+                speakerText.text = !string.IsNullOrEmpty(_currentSubtitle.SpeakerName)
+                    ? _currentSubtitle.SpeakerName + ":"
+                    : "";
+            }
+        }
+
+        public void HideImmediate()
+        {
+            CleanupTimers();
+            canvasGroup.alpha = 0f;
+            _isShowing = false;
+            canvasGroup.gameObject.SetActive(false);
+        }
+
+        private void CleanupTimers()
+        {
+            if(_displayTimer != null)
+            {
+                TimerSystem.Instance.StopTimer(_displayTimer.GetData().ID);
+                _displayTimer = null;
+            }
+            if (_fadeTimer != null)
+            {
+                TimerSystem.Instance.StopTimer(_fadeTimer.GetData().ID);
+                _fadeTimer = null;
+            }   
+        }
+    }
+
+}
--- Fast-and-fractured/Assets/FastAndFractured/MainMenu/Scripts/AccesibilitySettings/AccesibilitySettings.cs
+++ Fast-and-fractured/Assets/FastAndFractured/MainMenu/Scripts/AccesibilitySettings/AccesibilitySettings.cs
@@ -5,118 +5,125 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class AccesibilitySettings : MonoBehaviour
+namespace FastAndFractured
 {
-    [Header("Lenguage")]
-    [SerializeField] private Button nextLenguageButton;
-    [SerializeField] private Button previousLenguageButton;
-    private List<string> _languages = new List<string>();
-    private int _languageIndex = 0;
-    private const string LEANGUAGE_INDEX_KEY = "LanguageIndex";
-
-    [Header("ColorBlind")]
-    [SerializeField] ColorBlindModeController colorBlindModeController;
-    [SerializeField] private Toggle colorBlindToggle;
-    private const string COLORBLIND_KEY = "ColorBlindMode";
-
-    [Header("Subtitles")]
-    [SerializeField] private Toggle subtitlesToggle;
-    private const string SUBTITLES_KEY = "Substitles";
-    
-
-    private void Awake()
+    public class AccesibilitySettings : MonoBehaviour
     {
-        // if we decide to add more lenguages we can create a public array to fill all lenguages
-        _languages.Add("Catala");
-        _languages.Add("Espanol");
-        _languages.Add("English");
+        [Header("Lenguage")]
+        [SerializeField] private Button nextLenguageButton;
+        [SerializeField] private Button previousLenguageButton;
+        private List<string> _languages = new List<string>();
+        private int _languageIndex = 0;
+        private const string LEANGUAGE_INDEX_KEY = "LanguageIndex";
 
-        if (PlayerPrefs.HasKey(LEANGUAGE_INDEX_KEY))
+        [Header("ColorBlind")]
+        [SerializeField] ColorBlindModeController colorBlindModeController;
+        [SerializeField] private Toggle colorBlindToggle;
+        private const string COLORBLIND_KEY = "ColorBlindMode";
+
+        [Header("Subtitles")]
+        [SerializeField] private Toggle subtitlesToggle;
+        private const string SUBTITLES_KEY = "Substitles";
+
+
+        private void Awake()
         {
-            _languageIndex = PlayerPrefs.GetInt(LEANGUAGE_INDEX_KEY);
+            // if we decide to add more lenguages we can create a public array to fill all lenguages
+            _languages.Add("Catala");
+            _languages.Add("Espanol");
+            _languages.Add("English");
+
+            if (PlayerPrefs.HasKey(LEANGUAGE_INDEX_KEY))
+            {
+                _languageIndex = PlayerPrefs.GetInt(LEANGUAGE_INDEX_KEY);
+            }
+            LocalizationManager.Read();
+            SelectLanguage();
+
+            bool isColorBlindModeOn = PlayerPrefs.GetInt(COLORBLIND_KEY, 0) == 1;
+            colorBlindToggle.isOn = isColorBlindModeOn;
+
+            bool isSubtitlesOn = PlayerPrefs.GetInt(SUBTITLES_KEY, 0) == 1;
+            subtitlesToggle.isOn = isSubtitlesOn;
         }
-        LocalizationManager.Read();
-        SelectLanguage();
 
-        bool isColorBlindModeOn = PlayerPrefs.GetInt(COLORBLIND_KEY, 0) == 1;
-        colorBlindToggle.isOn = isColorBlindModeOn;
-
-        bool isSubtitlesOn = PlayerPrefs.GetInt(SUBTITLES_KEY, 0) == 1;
-        subtitlesToggle.isOn = isSubtitlesOn;
-    }
-
-    private void OnEnable()
-    {
-        nextLenguageButton?.onClick.AddListener(NextLanguage);
-        previousLenguageButton?.onClick.AddListener(PreviousLanguage);
-        colorBlindToggle?.onValueChanged.AddListener(OnColorBlindToggleChanged);
-    }
-
-    private void OnDisable()
-    {
-        nextLenguageButton?.onClick?.RemoveAllListeners();
-        previousLenguageButton?.onClick.RemoveAllListeners();
-        colorBlindToggle?.onValueChanged.RemoveAllListeners();
-        PlayerPrefs.Save();
-    }
-    
-    #region lenguage
-    private void SelectLanguage()
-    {
-        switch (_languageIndex)
+        private void OnEnable()
         {
-            case 0:
-                LocalizationManager.Language = "English";
-                break;
-            case 1:
-                LocalizationManager.Language = "Espanol";
-                break;
-            case 2:
-                LocalizationManager.Language = "Catala";
-                break;
+            nextLenguageButton?.onClick.AddListener(NextLanguage);
+            previousLenguageButton?.onClick.AddListener(PreviousLanguage);
+            colorBlindToggle?.onValueChanged.AddListener(OnColorBlindToggleChanged);
+            subtitlesToggle?.onValueChanged.AddListener(OnSubtitlesToggleChanged);
         }
-    }
 
-    public void NextLanguage()
-    {
-        _languageIndex++;
-        if (_languageIndex >= _languages.Count)
+        private void OnDisable()
         {
-            _languageIndex = 0;
+            nextLenguageButton?.onClick?.RemoveAllListeners();
+            previousLenguageButton?.onClick.RemoveAllListeners();
+            colorBlindToggle?.onValueChanged.RemoveAllListeners();
+            subtitlesToggle?.onValueChanged.RemoveAllListeners();
+            PlayerPrefs.Save();
         }
-        SelectLanguage();
-        PlayerPrefs.SetInt(LEANGUAGE_INDEX_KEY, _languageIndex);
-    }
-    public void PreviousLanguage()
-    {
-        _languageIndex--;
-        if (_languageIndex < 0)
+
+        #region lenguage
+        private void SelectLanguage()
         {
-            _languageIndex = _languages.Count - 1;
+            switch (_languageIndex)
+            {
+                case 0:
+                    LocalizationManager.Language = "English";
+                    break;
+                case 1:
+                    LocalizationManager.Language = "Espanol";
+                    break;
+                case 2:
+                    LocalizationManager.Language = "Catala";
+                    break;
+            }
         }
-        SelectLanguage();
-        PlayerPrefs.SetInt(LEANGUAGE_INDEX_KEY, _languageIndex);
+
+        public void NextLanguage()
+        {
+            _languageIndex++;
+            if (_languageIndex >= _languages.Count)
+            {
+                _languageIndex = 0;
+            }
+            SelectLanguage();
+            PlayerPrefs.SetInt(LEANGUAGE_INDEX_KEY, _languageIndex);
+        }
+        public void PreviousLanguage()
+        {
+            _languageIndex--;
+            if (_languageIndex < 0)
+            {
+                _languageIndex = _languages.Count - 1;
+            }
+            SelectLanguage();
+            PlayerPrefs.SetInt(LEANGUAGE_INDEX_KEY, _languageIndex);
+        }
+        #endregion
+
+        #region Color Blind
+
+        private void OnColorBlindToggleChanged(bool isOn)
+        {
+            PlayerPrefs.SetInt(COLORBLIND_KEY, isOn ? 1 : 0); // 1 true
+            PlayerPrefs.Save();
+            colorBlindModeController.UpdateColorBlindMode(isOn);
+
+        }
+
+        #endregion
+
+        #region Substitles
+
+        private void OnSubtitlesToggleChanged(bool isOn)
+        {
+            PlayerPrefs.SetInt(SUBTITLES_KEY, isOn ? 1 : 0);
+            PlayerPrefs.Save();
+            SubtitlesManager.Instance.ToggleSubtitles(isOn);
+        }
+        #endregion
     }
-    #endregion
-
-    #region Color Blind
-
-    private void OnColorBlindToggleChanged(bool isOn)
-    {
-        PlayerPrefs.SetInt(COLORBLIND_KEY, isOn ? 1 : 0); // 1 true
-        PlayerPrefs.Save();
-        colorBlindModeController.UpdateColorBlindMode(isOn);
- 
-    }
-
-    #endregion
-
-    #region Substitles
-    
-    private void OnSubtitlesToggleChanged(bool isOn)
-    {
-        PlayerPrefs.SetInt(SUBTITLES_KEY, isOn ? 1 : 0);
-        PlayerPrefs.Save();
-    }
-    #endregion
 }
+
