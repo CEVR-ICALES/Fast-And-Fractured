a97f01546a22cde7419051a7c136194963a5d3ac
Álvaro Miguel Bárcena <alvaro207mb@gmail.com>
2025-04-09 19:34:33 +0200

    input remaping progress

--- Fast-and-fractured/Assets/Samples/Input System/1.11.2/Rebinding UI/GamepadIconsExample.cs
+++ Fast-and-fractured/Assets/Samples/Input System/1.11.2/Rebinding UI/GamepadIconsExample.cs
@@ -15,6 +15,7 @@ namespace UnityEngine.InputSystem.Samples.RebindUI
     {
         public GamepadIcons xbox;
         public GamepadIcons ps4;
+        public GamepadIcons keyboard;
 
         protected void OnEnable()
         {
@@ -37,6 +38,8 @@ namespace UnityEngine.InputSystem.Samples.RebindUI
                 icon = ps4.GetSprite(controlPath);
             else if (InputSystem.IsFirstLayoutBasedOnSecond(deviceLayoutName, "Gamepad"))
                 icon = xbox.GetSprite(controlPath);
+            else if (InputSystem.IsFirstLayoutBasedOnSecond(deviceLayoutName, "Keyboard"))
+                icon = keyboard.GetSprite(controlPath);
 
             var textComponent = component.bindingText;
 
@@ -105,6 +108,122 @@ namespace UnityEngine.InputSystem.Samples.RebindUI
                     case "rightStick": return rightStick;
                     case "leftStickPress": return leftStickPress;
                     case "rightStickPress": return rightStickPress;
+                }
+                return null;
+            }
+        }
+
+        [Serializable]
+        public struct KeyboardIcons
+        {
+            public Sprite KeyA;
+            public Sprite KeyB;
+            public Sprite KeyC;
+            public Sprite KeyD;
+            public Sprite KeyE;
+            public Sprite KeyF;
+            public Sprite KeyG;
+            public Sprite KeyH;
+            public Sprite KeyI;
+            public Sprite KeyJ;
+            public Sprite KeyK;
+            public Sprite KeyL;
+            public Sprite KeyM;
+            public Sprite KeyN;
+            public Sprite KeyO;
+            public Sprite KeyP;
+            public Sprite KeyQ;
+            public Sprite KeyR;
+            public Sprite KeyS;
+            public Sprite KeyT;
+            public Sprite KeyU;
+            public Sprite KeyV;
+            public Sprite KeyW;
+            public Sprite KeyX;
+            public Sprite KeyY;
+            public Sprite KeyZ;
+
+            public Sprite Key0;
+            public Sprite Key1;
+            public Sprite Key2;
+            public Sprite Key3;
+            public Sprite Key4;
+            public Sprite Key5;
+            public Sprite Key6;
+            public Sprite Key7;
+            public Sprite Key8;
+            public Sprite Key9;
+
+            public Sprite KeyEnter;
+            public Sprite KeySpace;
+            public Sprite KeyShift;
+            public Sprite KeyCtrl;
+            public Sprite KeyAlt;
+            public Sprite KeyTab;
+            public Sprite KeyBackspace;
+            public Sprite KeyCapsLock;
+            public Sprite KeyDelete;
+            public Sprite KeyArrowUp;
+            public Sprite KeyArrowDown;
+            public Sprite KeyArrowLeft;
+            public Sprite KeyArrowRight;
+
+
+            public Sprite GetSprite(string controlPath)
+            {
+                switch (controlPath)
+                {
+                    case "a": return KeyA;
+                    case "b": return KeyB;
+                    case "c": return KeyC;
+                    case "d": return KeyD;
+                    case "e": return KeyE;
+                    case "f": return KeyF;
+                    case "g": return KeyG;
+                    case "h": return KeyH;
+                    case "i": return KeyI;
+                    case "j": return KeyJ;
+                    case "k": return KeyK;
+                    case "l": return KeyL;
+                    case "m": return KeyM;
+                    case "n": return KeyN;
+                    case "o": return KeyO;
+                    case "p": return KeyP;
+                    case "q": return KeyQ;
+                    case "r": return KeyR;
+                    case "s": return KeyS;
+                    case "t": return KeyT;
+                    case "u": return KeyU;
+                    case "v": return KeyV;
+                    case "w": return KeyW;
+                    case "x": return KeyX;
+                    case "y": return KeyY;
+                    case "z": return KeyZ;
+
+                    case "0": return Key0;
+                    case "1": return Key1;
+                    case "2": return Key2;
+                    case "3": return Key3;
+                    case "4": return Key4;
+                    case "5": return Key5;
+                    case "6": return Key6;
+                    case "7": return Key7;
+                    case "8": return Key8;
+                    case "9": return Key9;
+
+                    case "enter": return KeyEnter;
+                    case "space": return KeySpace;
+                    case "leftShift": return KeyShift;
+                    case "leftCtrl": return KeyCtrl;
+                    case "leftAlt": return KeyAlt;
+                    case "tab": return KeyTab;
+                    case "backspace": return KeyBackspace;
+                    case "capsLock": return KeyCapsLock;
+                    case "delete": return KeyDelete;
+                    case "upArrow": return KeyArrowUp;
+                    case "downArrow": return KeyArrowDown;
+                    case "leftArrow": return KeyArrowLeft;
+                    case "rightArrow": return KeyArrowRight;
                 }
                 return null;
             }
