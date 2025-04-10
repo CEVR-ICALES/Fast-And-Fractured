574571dd2dc89152760bc7a3ea16b75aee6ac8ad
Iker Boluda <ikerbltablet@gmail.com>
2025-04-10 17:23:51 +0200

    Minimap working with level controller and has icon for selected character

--- Fast-and-fractured/Assets/FastAndFractured/!Common/Managers/Scripts/LevelController.cs
+++ Fast-and-fractured/Assets/FastAndFractured/!Common/Managers/Scripts/LevelController.cs
@@ -59,6 +59,7 @@ namespace FastAndFractured
         [Tooltip("In case there is not that much variety of characters un characters data, repeting will be allowed.")]
         [SerializeField] private bool repeatCharacters = true;
         [SerializeField] private bool stormInDebugMode = false;
+        public GameObject playerReference;
 
 
         private const char DELIMITER_CHAR_FOR_CHARACTER_NAMES_CODE = '_';
@@ -245,6 +246,7 @@ namespace FastAndFractured
 
                     _inGameCharacters.Add(playerCar);
                     _playerBindingInputs = player.GetComponentInChildren<CarMovementController>();
+                    playerReference = playerCar;
                 }
                 for(;charactersCount < allCharacters; charactersCount++)
                 {
