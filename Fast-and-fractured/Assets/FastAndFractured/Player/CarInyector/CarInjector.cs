8f2ce40ad26e09bd12d66a75fa18e8fb06fe9e16
CortesJoan <cortesjoancontact@gmail.com>
2025-04-10 18:12:49 +0200

    CarInjector updated AI related values

--- Fast-and-fractured/Assets/FastAndFractured/Player/CarInyector/CarInjector.cs
+++ Fast-and-fractured/Assets/FastAndFractured/Player/CarInyector/CarInjector.cs
@@ -32,12 +32,13 @@ public class CarInjector : MonoBehaviour
         var injectedCar = Instantiate(prefab, this.transform.position, Quaternion.identity, transform);
         var controllers = GetComponentsInChildren<Controller>();
         var positionConstraints = transform.GetComponentsInChildren<IConstraint>();
-        //    var parentConstraints = transform.GetComponentsInChildren<ParentConstraint>();
+
+        var constraintSource = new ConstraintSource();
+        constraintSource.sourceTransform = injectedCar.transform;
+        constraintSource.weight = MAX_WEIGHT;
+
         foreach (var constraint in positionConstraints)
         {
-            var constraintSource = new ConstraintSource();
-            constraintSource.sourceTransform = injectedCar.transform;
-            constraintSource.weight = MAX_WEIGHT;
             constraint.SetSources(new List<ConstraintSource>() { constraintSource });
         }
 
@@ -50,11 +51,12 @@ public class CarInjector : MonoBehaviour
                 controller.AddBehaviour(mono);
             }
         }
-        if (GetComponent<EnemyAIBrain>() == null)
+        if(TryGetComponent<EnemyAIBrain>(out EnemyAIBrain enemyAIBrain))
         {
-            Debug.Log("sd");
+            enemyAIBrain.InstallAIParameters(injectedCar.GetComponent<StatsController>().CharacterData.AIParameters);
+        }else{
             injectedCar.GetComponent<CarMovementController>().speedOverlay = GameObject.Find("SpeedOverlay").GetComponentInChildren<TextMeshProUGUI>();
-        }
+        } 
 
         return injectedCar;
     }
