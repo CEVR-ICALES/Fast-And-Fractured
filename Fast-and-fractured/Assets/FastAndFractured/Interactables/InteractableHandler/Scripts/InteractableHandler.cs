cf0d2e582019744bc88f6cabbc695d054271b8d1
Pau <68166681+PauSolT@users.noreply.github.com>
2025-04-24 17:13:53 +0200

    remove code from interactable handler

--- Fast-and-fractured/Assets/FastAndFractured/Interactables/InteractableHandler/Scripts/InteractableHandler.cs
+++ Fast-and-fractured/Assets/FastAndFractured/Interactables/InteractableHandler/Scripts/InteractableHandler.cs
@@ -16,12 +16,11 @@ namespace FastAndFractured
         [SerializeField] float itemCooldownAfterPick = 2f;
 
         List<GameObject> _shuffledActivePool = new();
+        public List<GameObject> ShuffledActivePool => _shuffledActivePool;
         List<GameObject> _interactablesOnCooldown = new();
+        SkinUnlockHandler skinUnlockHandler;
 
-        string _playerSelected = "";
 
-        const int MAX_SKINS_TO_SPAWN = 2;
-        const int PIECES_TO_UNLOCK_SKIN = 5;
         protected override void Awake()
         {
             base.Awake();
@@ -32,19 +31,27 @@ namespace FastAndFractured
                 interactable.disableGameObjectOnInteract = true;
                 interactable.onInteract.AddListener(RemoveInteractableFromPool);
             }
+
+            skinUnlockHandler = GetComponent<SkinUnlockHandler>();
         }
 
         void Start()
         {
-            //Split to get only the name of the character, regardless of the skin
-            _playerSelected = PlayerPrefs.GetString("Selected_Player").Split("_")[0];
+            if (!skinUnlockHandler)
+            {
+                Debug.LogWarning("InteractableHandler doesn't have SkinUnlockHandler.\n" +
+                    "GET THAT INTERACTABLEHANDLER A SKINUNLOCKHANDLER NOW!!!!");
+            } else
+            {
+                skinUnlockHandler.Init();
+            }
             MakeInitialPool();
         }
 
         void MakeInitialPool()
         {
             _shuffledActivePool = interactablesToToggle.OrderBy(_ => UnityEngine.Random.Range(0, interactablesToToggle.Length)).ToList();
-            CheckDespawnSkinInteractables();
+            skinUnlockHandler.CheckDespawnSkinInteractables();
             UpdateVisibleInteractableList();
         }
 
@@ -134,85 +141,6 @@ namespace FastAndFractured
         }
         #endregion
         #region Skins
-        private List<string> GetPlayerUnlockableSkins()
-        {
-            List<string> unlockableSkins = new List<string>();
-            //Get the number of skins of the character
-            //that the player is using
-            int numOfSkins = LevelController.Instance.playerReference.GetComponentInChildren<StatsController>().SkinCount;
-
-            //Return null if player selected returns empty
-            if (_playerSelected == "")
-            {
-                Debug.LogError("No player selected to get skins to unlock. " +
-                    "HELLO?????!!!\n" +
-                    "is that even possible...?");
-                return null;
-            }
-
-            for (int i = 0; i < numOfSkins; i++)
-            {
-                string skinToCheck = _playerSelected + "_" + i; 
-                if (PlayerPrefs.GetInt(skinToCheck) < PIECES_TO_UNLOCK_SKIN)
-                {
-                    unlockableSkins.Add(skinToCheck);
-                }
-            }
-            return unlockableSkins;
-
-        }
-
-        private bool HasSkinsToUnlock()
-        {
-            List<string> skinsToUnlock = GetPlayerUnlockableSkins();
-            return skinsToUnlock != null && skinsToUnlock.Count > 0;
-        }
-
-        private void CheckDespawnSkinInteractables()
-        {
-            // If there are no skins to unlock
-            //Remove them from the list and deactivate them
-            if (!HasSkinsToUnlock())
-            {
-                List<GameObject> skinInteractables = _shuffledActivePool.FindAll(s => s.GetComponentInChildren<SkinUnlockerInteractable>() != null);
-                foreach (GameObject interact in skinInteractables)
-                {
-                    if (interact.GetComponentInChildren<SkinUnlockerInteractable>())
-                    {
-                        _shuffledActivePool.Remove(interact);
-                        interact.SetActive(false);
-                    }
-                }
-                return;
-            } 
-            List<string> skinsToUnlock = GetPlayerUnlockableSkins();
-
-            //Random to select a random skin to unlock
-            //Random starts at 1 because base skin is 0 and is unlocked by default
-            //+1 is added because maximum on int is exclusive
-            int skinSelected = UnityEngine.Random.Range(1, skinsToUnlock.Count+1);
-            string playerSkinId = _playerSelected + "_" + skinSelected;
-
-            int unlockedPieces = PlayerPrefs.GetInt(playerSkinId, 0);
-            //Calculate how many pieces are needed to unlock the skin
-            //If it is less than MAX_SKINS_TO_SPAWN, remove the necessary interactables
-            int interactablesToDespawn = unlockedPieces - PIECES_TO_UNLOCK_SKIN + MAX_SKINS_TO_SPAWN;
-            //int interactablesToDespawn = 2;
-            for (int i = 0; i < interactablesToDespawn; i++)
-            {
-                GameObject skin = _shuffledActivePool.Find(s => s.GetComponentInChildren<SkinUnlockerInteractable>() != null);
-                _shuffledActivePool.Remove(skin);
-                skin.SetActive(false);
-            }
-            foreach (GameObject interact in _shuffledActivePool)
-            {
-                if (interact.TryGetComponent(out SkinUnlockerInteractable skin))
-                {
-                    skin.SkinToUnlock = playerSkinId;
-                }
-            }
-        }
-
         public void DestroySkinInteractable(GameObject skinInteractable)
         {
             _shuffledActivePool.Remove(skinInteractable);
