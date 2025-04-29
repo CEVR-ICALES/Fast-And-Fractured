f291e9e6201332f0f9ec087ab81c6013f74adabb
ImAlvaroMb <alvaro207mb@gmail.com>
2025-04-29 15:44:45 +0200

    started fixing brightness settting

--- Fast-and-fractured/Assets/FastAndFractured/!Common/Managers/Scripts/BrightnessManager.cs
+++ Fast-and-fractured/Assets/FastAndFractured/!Common/Managers/Scripts/BrightnessManager.cs
@@ -7,17 +7,26 @@ using Utilities;
 public class BrightnessManager : AbstractSingleton<BrightnessManager>
 {
     #region Variables
-    private float _brightness = 1f;
-    private const float MIN_GAMMA_THRESHOLD_VALUE = -0.7f;
-    private const float MAX_GAMMA_THRESHOLD_VALUE = 0.7f;
+    [SerializeField] private Volume boxVolume;
+    private ShadowsMidtonesHighlights _shadowMitonesHighlights;
+    private float _brightness;
+
+    private const float MIN_MIDTONE_MODIFIER = -0.55f;
+    private const float MAX_MIDTONE_MODIFIER = 0.55f;
+    private const float DEFAULT_MIDTONES_MODIFIER = 1f;
     private const string PLAYER_PREF_BRIGHTNESS_STRING = "Brightness";
     #endregion
 
     protected override void Awake()
     {
         base.Awake();
-        _brightness = PlayerPrefs.GetFloat(PLAYER_PREF_BRIGHTNESS_STRING, _brightness);
+        _brightness = PlayerPrefs.GetFloat(PLAYER_PREF_BRIGHTNESS_STRING, DEFAULT_MIDTONES_MODIFIER);
         SceneManager.sceneLoaded += OnSceneLoaded;
+        if(boxVolume.profile.TryGet(out _shadowMitonesHighlights))
+        {
+            _shadowMitonesHighlights.active = true;
+            ApplyBrightnessToScene();
+        }
     }
 
     private void OnDestroy()
@@ -32,26 +41,24 @@ public class BrightnessManager : AbstractSingleton<BrightnessManager>
 
     public void SetBrightness(float value)
     {
+        Debug.Log(value);
         _brightness = value;
         PlayerPrefs.SetFloat(PLAYER_PREF_BRIGHTNESS_STRING, _brightness);
         PlayerPrefs.Save();
-
-        ApplyBrightnessToScene();
+       ApplyBrightnessToScene();
     }
 
     private void ApplyBrightnessToScene()
     {
-        Volume globalVolume = FindObjectOfType<Volume>();
 
-        if (globalVolume == null || globalVolume.profile == null) return;
-
-        LiftGammaGain liftGammaGain;
-
-        if (!globalVolume.profile.TryGet(out liftGammaGain))
-            liftGammaGain = globalVolume.profile.Add<LiftGammaGain>(true);
-
-        float gamma = Mathf.Lerp(MIN_GAMMA_THRESHOLD_VALUE, MAX_GAMMA_THRESHOLD_VALUE, _brightness);
-
-        liftGammaGain.gamma.Override(new Vector4(1f, 1f, 1f, gamma));
+        float midtonesValue = Mathf.Lerp(MIN_MIDTONE_MODIFIER, MAX_MIDTONE_MODIFIER, _brightness);
+        _shadowMitonesHighlights.midtones.overrideState = true;
+        _shadowMitonesHighlights.midtones.value = new Vector4(
+            1f,
+            1f,
+            1f,
+            midtonesValue
+            );
     }
+
 }
