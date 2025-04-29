03fbfb73ad2e7c2ab01d595398f581235b4bbfec
Adrián Ontiveros Cruz <adrionticruz@gmail.com>
2025-04-29 18:06:36 +0200

    stop after timeline evaluate

--- Fast-and-fractured/Assets/FastAndFractured/MainMenu/Scripts/MenuSkipInitialCutscene.cs
+++ Fast-and-fractured/Assets/FastAndFractured/MainMenu/Scripts/MenuSkipInitialCutscene.cs
@@ -26,6 +26,7 @@ public class MenuSkipInitialCutscene : MonoBehaviour
         {
             timeLine.time = timeLine.duration;
             timeLine.Evaluate();
+            timeLine.Stop();
             skipText.SetActive(false);
         }
     }
