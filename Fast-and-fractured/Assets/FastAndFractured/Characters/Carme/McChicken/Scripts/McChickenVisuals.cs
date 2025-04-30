8d4a2b6a09c9cc73f3d4295bba3aa0b8306c56eb
ImAlvaroMb <alvaro207mb@gmail.com>
2025-04-30 10:44:14 +0200

    mcchicken spawn visual changes

--- Fast-and-fractured/Assets/FastAndFractured/Characters/Carme/McChicken/Scripts/McChickenVisuals.cs
+++ Fast-and-fractured/Assets/FastAndFractured/Characters/Carme/McChicken/Scripts/McChickenVisuals.cs
@@ -4,12 +4,25 @@ using UnityEngine;
 
 public class McChickenVisuals : MonoBehaviour
 {
+    [Header("Particles")]
     [SerializeField] private ParticleSystem _spawnFeathersVfx;
 
+    [Header("Models")]
+    [SerializeField] private GameObject chickenModel;
+    [SerializeField] private GameObject eggModel;
+
+
     //this scripts will also have animations logic
+    public void OnEggLaunched()
+    {
+        chickenModel.SetActive(false);
+        eggModel.SetActive(true);
+    }
 
     public void OnLand()
     {
+        eggModel.SetActive(false);
+        chickenModel.SetActive(true);
         _spawnFeathersVfx.Play();
     }
 }
