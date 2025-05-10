be1d2ea1dd2c4b4c1e5e3fea0ac2561152664fda
Carlos Roig Garcia <carlos.roig.garcia9@gmail.com>
2025-05-10 18:27:09 +0200

    ChangedLevelcontrollerCreatingAFalseThirdSkin

--- Fast-and-fractured/Assets/FastAndFractured/!Common/Managers/Scripts/LevelController.cs
+++ Fast-and-fractured/Assets/FastAndFractured/!Common/Managers/Scripts/LevelController.cs
@@ -315,6 +315,10 @@ namespace FastAndFractured
                 characterSkinCount = DEFAULT_SKIN + 1;
                 _allCharactersNameCode.Add(character.CharacterName + DELIMITER_CHAR_FOR_CHARACTER_NAMES_CODE + DEFAULT_SKIN.ToString());
                 _characterSelectedLimit.Add(character.CharacterName, 0);
+                if (character.CarWithSkinsPrefabs.Count < LIMIT_OF_SAME_CHARACTER_SPAWNED)
+                {
+                    _allCharactersNameCode.Add(character.CharacterName + DELIMITER_CHAR_FOR_CHARACTER_NAMES_CODE + DEFAULT_SKIN.ToString());
+                }
                 foreach (var characterSkin in character.CarWithSkinsPrefabs)
                 {
                     _allCharactersNameCode.Add(character.CharacterName + DELIMITER_CHAR_FOR_CHARACTER_NAMES_CODE + characterSkinCount.ToString());
