16f8abae1edfae097eb018d37b0fa1c37179da23
ImAlvaroMb <alvaro207mb@gmail.com>
2025-05-11 13:27:47 +0200

    skin unlock working?

--- Fast-and-fractured/Assets/FastAndFractured/Interactables/SkinUnlockInteractable/Scripts/SkinUnlockerInteractable.cs
+++ Fast-and-fractured/Assets/FastAndFractured/Interactables/SkinUnlockInteractable/Scripts/SkinUnlockerInteractable.cs
@@ -10,40 +10,54 @@ namespace FastAndFractured
         private string _skinToUnlock;
         public string SkinToUnlock { get => _skinToUnlock; set => _skinToUnlock = value; }
 
-
-
         private const string CARME_NAME = "Carme";
         private const string JOSEFINO_NAME = "Josefino";
         private const string PEPE_NAME = "Pepe";
         private const string MARIA_ANTONIA_NAME = "Maria";
         [SerializeField] List<GameObject> visuals;
 
-        public void InitializeUnlockeable(string skinToUnlock)
+        //private void OnEnable()
+        //{
+        //    if(SkinUnlockHandler.Instance != null)
+        //        SkinUnlockHandler.Instance.CheckDespawnSkinInteractables();
+        //}
+
+        public void ChangeVisual(string skinToUnlock)
         {
+            DisableAllVisuals();
             _skinToUnlock = skinToUnlock;
+            Debug.Log(skinToUnlock);
             GameObject player = LevelController.Instance.playerReference;
             if (player != null)
             {
                 switch (player.name)
                 {
                     case string name when name.Contains(CARME_NAME):
-                        visuals[1].SetActive(true);
+                        visuals[0].SetActive(true);
                         break;
                     case string name when name.Contains(PEPE_NAME):
-                        visuals[2].SetActive(true);
+                        visuals[1].SetActive(true);
                         break;
 
                     case string name when name.Contains(MARIA_ANTONIA_NAME):
-                        visuals[3].SetActive(true);
+                        visuals[2].SetActive(true);
                         break;
 
                     case string name when name.Contains(JOSEFINO_NAME):
-                        visuals[4].SetActive(true);
+                        visuals[3].SetActive(true);
                         break;
                     default:
                         visuals[0].SetActive(true);
                         break;
                 }
+            }
+        }
+
+        private void DisableAllVisuals()
+        {
+            foreach(GameObject obj in visuals)
+            {
+                obj.SetActive(false);
             }
         }
         public override void OnInteract(GameObject interactionFrom, GameObject interactionTo)
