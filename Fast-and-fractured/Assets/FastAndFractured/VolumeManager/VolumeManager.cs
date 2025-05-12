ae69e996cf0280c6180d12a21de7bc9699f5f318
CortesJoan <cortesjoancontact@gmail.com>
2025-05-12 18:22:57 +0200

    Fix sandstorm

--- Fast-and-fractured/Assets/FastAndFractured/VolumeManager/VolumeManager.cs
+++ Fast-and-fractured/Assets/FastAndFractured/VolumeManager/VolumeManager.cs
@@ -3,17 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.Rendering;
+using UnityEngine.Rendering.HighDefinition;
 using Utilities;
 
 public class VolumeManager : AbstractSingleton<VolumeManager>
 {
+    private const float FOG_VALUE = 500f;
     [SerializeField] private List<GameObject> volumeList;
-    [SerializeField] private bool randomStart =true;
+    [SerializeField] private bool randomStart = true;
     [SerializeField] private GameObject currentVolumeObject;
 
     public List<GameObject> VolumeList { get => volumeList; set => volumeList = value; }
-    public GameObject CurrentVolumeGameObject  => currentVolumeObject ;
-    public Volume CurrentVolumeComponent => currentVolumeObject.GetComponentInChildren<Volume>();    
+    public GameObject CurrentVolumeGameObject => currentVolumeObject;
+    public Volume CurrentVolumeComponent => currentVolumeObject.GetComponentInChildren<Volume>();
 
     protected override void Awake()
     {
@@ -28,7 +30,7 @@ public class VolumeManager : AbstractSingleton<VolumeManager>
         ChangeCurrentVolume(volumeList.First());
         if (randomStart)
         {
-            int selected =  Random.Range(0, volumeList.Count);
+            int selected = Random.Range(0, volumeList.Count);
             ChangeCurrentVolume(volumeList[selected]);
         }
     }
@@ -41,13 +43,32 @@ public class VolumeManager : AbstractSingleton<VolumeManager>
         }
         currentVolumeObject = newVolume;
         currentVolumeObject?.SetActive(true);
+        if (CurrentVolumeComponent.profile.TryGet<Fog>(out Fog fogComponent))
+        {
+             fogComponent.active = true;
+        //    fogComponent.SetAllOverridesTo(true);
+            fogComponent.enableVolumetricFog.overrideState = true;
+            fogComponent.enableVolumetricFog.value = true;
+            fogComponent.enableVolumetricFog.Override(true);
+           // fogComponent.depthExtent.Override( FOG_VALUE);
+            fogComponent.enableVolumetricFog.overrideState = true; // Enable the override for this parameter
+            fogComponent.enableVolumetricFog.value = true;         // Set the value
+
+        
+
+            // If you want to ensure fog itself is "on" (though 'active = true' usually handles this for the override)
+            fogComponent.enabled.overrideState = true; // This refers to the general on/off toggle for the fog effect
+            fogComponent.enabled.value = true;
+            fogComponent.Override(fogComponent, 0);
+
+        }
     }
     public void ChangeCurrentVolume(int volumeIndex)
     {
         ChangeCurrentVolume(VolumeList.ElementAtOrDefault(volumeIndex));
     }
     public List<string> GetVolumeNames()
-    { 
+    {
         List<string> result = new List<string>();
         foreach (var volumeGameobject in VolumeList)
         {
