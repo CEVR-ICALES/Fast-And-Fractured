b85f6f5c8a583fda77fbbf1ba2d1e90d757a542e
CortesJoan <cortesjoancontact@gmail.com>
2025-05-22 18:44:01 +0200

    Update LevelControllerButBetter.cs

--- Fast-and-fractured/Assets/FastAndFractured/!Common/Managers/LevelController/LevelControllerButBetter.cs
+++ Fast-and-fractured/Assets/FastAndFractured/!Common/Managers/LevelController/LevelControllerButBetter.cs
@@ -206,7 +206,6 @@ namespace FastAndFractured
                         if (aiDebugChanger) aiDebugChanger.NextState();
                     }
                 }
-                else { Debug.LogError("aaaaaaaaaaaaaaaaaaaaaaaaaa"); }
             }
         }
 
