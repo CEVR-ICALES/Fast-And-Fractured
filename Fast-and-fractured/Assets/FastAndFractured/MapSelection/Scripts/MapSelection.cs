0dee01ba97c79beabc6b4bfcf5d31c56b31eac07
iker908 <ikerbltablet@gmail.com>
2025-07-08 14:50:36 +0200

    changed map selection location in menu

--- Fast-and-fractured/Assets/FastAndFractured/MapSelection/Scripts/MapSelection.cs
+++ Fast-and-fractured/Assets/FastAndFractured/MapSelection/Scripts/MapSelection.cs
@@ -1,6 +1,5 @@
 using UnityEngine;
 using UnityEngine.UI;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem.UI;
@@ -13,82 +12,77 @@ namespace FastAndFractured
         [SerializeField] private Sprite[] spriteList;
         [SerializeField] private GameObject panel;
         [SerializeField] private GameObject returnButton;
-        private List<Button> buttons = new List<Button>();
-        private int selectedIndex = -1;
+        [SerializeField] private int min_animation_steps = 10;
+        [SerializeField] private int max_animation_steps = 15;
+        private readonly List<Button> buttons = new();
+        private int selectedIndex = 0;
 
-        private bool isAnimating = false;
-        private int animationSteps = 0;
-        private int animationTargetSteps = 0;
-        private float animationTimer = 0f;
-        private int animationCurrentIndex = 0;
+        private bool isAnimating;
+        private int animationSteps;
+        private int animationTargetSteps;
+        private float animationTimer;
+        private int animationCurrentIndex;
+
         private EventSystem eventSystem;
         private InputSystemUIInputModule inputModule;
         private const float ANIMATION_INTERVAL = 0.5f;
-        private const int MIN_ANIMATION_STEPS = 10;
-        private const int MAX_ANIMATION_STEPS = 15;
-
+        
         void Start()
         {
             eventSystem = EventSystem.current;
             inputModule = eventSystem.GetComponent<InputSystemUIInputModule>();
             panel.SetActive(false);
+
             buttons.AddRange(GetComponentsInChildren<Button>());
+
             for (int i = 0; i < buttons.Count; i++)
             {
                 int idx = i;
-                EventTrigger trigger = buttons[i].gameObject.GetComponent<EventTrigger>();
-                if (trigger == null)
-                    trigger = buttons[i].gameObject.AddComponent<EventTrigger>();
-
+                var trigger = buttons[i].gameObject.GetComponent<EventTrigger>() ?? buttons[i].gameObject.AddComponent<EventTrigger>();
                 var entry = new EventTrigger.Entry { eventID = EventTriggerType.PointerEnter };
-                entry.callback.AddListener((eventData) => { UpdateSelectedMapImage(idx); });
+                entry.callback.AddListener((_) => UpdateSelectedMapImage(idx));
                 trigger.triggers.Add(entry);
             }
-            UpdateSelectedMapImage(0);
+
+            UpdateSelectedMapImage(selectedIndex);
         }
 
         void Update()
         {
-            if (isAnimating)
-            {
-                animationTimer += Time.deltaTime;
-                if (animationTimer >= ANIMATION_INTERVAL)
-                {
-                    animationTimer = 0f;
-                    if (selectedIndex >= 0)
-                        DeselectButton(selectedIndex);
+            if (!isAnimating) return;
 
-                    animationCurrentIndex = (animationCurrentIndex + 1) % buttons.Count;
-                    SelectButton(animationCurrentIndex);
-                    selectedIndex = animationCurrentIndex;
+            animationTimer += Time.deltaTime;
+            if (animationTimer < ANIMATION_INTERVAL) return;
 
-                    animationSteps++;
+            animationTimer = 0f;
+            DeselectButton(selectedIndex);
 
-                    if (animationSteps >= animationTargetSteps)
-                    {
-                        isAnimating = false;
+            animationCurrentIndex = (animationCurrentIndex + 1) % buttons.Count;
+            SelectButton(animationCurrentIndex);
+            selectedIndex = animationCurrentIndex;
+            animationSteps++;
 
-                        if (selectedIndex >= 0)
-                            DeselectButton(selectedIndex);
+            if (animationSteps < animationTargetSteps) return;
 
-                        SelectButton(selectedIndex);
-                        buttons[selectedIndex].onClick.Invoke();
-                        inputModule.enabled = true;
-                    }
-                }
-            }
+            isAnimating = false;
+            DeselectButton(selectedIndex);
+            SelectButton(selectedIndex);
+            buttons[selectedIndex].onClick.Invoke();
+            inputModule.enabled = true;
         }
 
         public void RandomMap()
         {
             if (buttons.Count == 0) return;
+
             panel.SetActive(true);
             inputModule.enabled = false;
             returnButton.SetActive(false);
-            animationTargetSteps = Random.Range(MIN_ANIMATION_STEPS, MAX_ANIMATION_STEPS + 1);
+
+            animationTargetSteps = Random.Range(min_animation_steps, max_animation_steps + 1);
             animationSteps = 0;
             animationTimer = 0f;
-            animationCurrentIndex = selectedIndex >= 0 ? selectedIndex : 0;
+            animationCurrentIndex = selectedIndex;
             isAnimating = true;
         }
 
@@ -97,20 +91,19 @@ namespace FastAndFractured
             buttons[index].Select();
             UpdateSelectedMapImage(index);
         }
+
         private void DeselectButton(int index)
         {
             if (EventSystem.current.currentSelectedGameObject == buttons[index].gameObject)
-            {
                 EventSystem.current.SetSelectedGameObject(null);
-            }
         }
+
         private void UpdateSelectedMapImage(int index)
         {
-            if (selectedMapImage != null && buttons.Count > index && index >= 0)
+            if (selectedMapImage && index >= 0 && index < spriteList.Length)
             {
-                Sprite sprite = spriteList[index];
-                if (sprite != null)
-                    selectedMapImage.sprite = sprite;
+                var sprite = spriteList[index];
+                if (sprite) selectedMapImage.sprite = sprite;
             }
         }
     }
