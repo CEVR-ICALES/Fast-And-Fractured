e1a44a99498d8f09efbbf3dabaf7e95bf76e8d3d
Carlos Roig Garcia <carlos.roig.garcia9@gmail.com>
2025-07-18 18:38:50 +0200

    WorkingTrampolineForCars

--- Fast-and-fractured/Assets/FastAndFractured/Trampoline/ParabolicImpulse.cs
+++ Fast-and-fractured/Assets/FastAndFractured/Trampoline/ParabolicImpulse.cs
@@ -4,15 +4,19 @@ namespace FastAndFractured {
     public class ParabolicImpulse : MonoBehaviour
     {
         [SerializeField]
+        private bool fixedVY = false;
+        [SerializeField]
         private float initialVy_impulse = 100f;
+        [Range(1.2f, 5f)]
+        [SerializeField]
+        private float maxVelocityMultiplicatorFactor = 2.5f;
         [SerializeField]
         private float angle = 45f;
         [SerializeField]
-        private float start_impulse_time = 0.75f;
-        [SerializeField]
         private float countdown = 0.5f;
         [SerializeField]
-        private Vector3 launchPosition = Vector3.zero;
+        private float maxThrowableWeight = 2000f;
+
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         void Start()
         {
@@ -31,22 +35,8 @@ namespace FastAndFractured {
             {
                 var characterCarMovementController = characterCar.GetComponent<CarMovementController>();
                 Vector3 currentVelocity = characterCar.Rb.linearVelocity;
-                //characterCar.Rb.linearVelocity = Vector3.zero;
-                float moveSpeed = ((launchPosition + transform.position).magnitude - other.transform.position.magnitude)/start_impulse_time;
-                Vector3 direction = (launchPosition + transform.position) - other.transform.position;
-                TimerSystem.Instance.CreateTimer(start_impulse_time, onTimerDecreaseUpdate: (float time) =>
-                {
-                    MoveCarToLaunchPosition(other.transform,moveSpeed,direction);
-                }, onTimerDecreaseComplete: () =>
-                {
-                    CarImpulse(characterCar, currentVelocity);
-                });
+                CarImpulse(characterCar, currentVelocity);
             }
-        }
-
-        private void MoveCarToLaunchPosition(Transform car,float moveSpeed, Vector3 direction)
-        {
-            car.position = car.position + direction * moveSpeed * Time.deltaTime;
         }
 
         private void CarImpulse(PhysicsBehaviour car,Vector3 startingVelocity)
@@ -55,11 +45,11 @@ namespace FastAndFractured {
             float startingVelocityZ = startingVelocity.z;
 
             float forwardVelocityMagnitude = Mathf.Sqrt(Mathf.Pow(startingVelocityX, 2) + Mathf.Pow(startingVelocityZ, 2) + 2 * startingVelocityZ * startingVelocityX * Mathf.Cos(45));
+            forwardVelocityMagnitude *= maxVelocityMultiplicatorFactor - (1 - (car.Rb.mass/maxThrowableWeight));
             float initialSpeedNeeded = forwardVelocityMagnitude / Mathf.Cos(angle * Mathf.Deg2Rad);
-            //float startingVelocityUp = initialSpeedNeeded * Mathf.Sin(angle * Mathf.Deg2Rad);
-
-            car.Rb.linearVelocity = forwardVelocityMagnitude * car.transform.forward + initialVy_impulse * Vector3.up;
-
+            float startingVelocityUp = initialSpeedNeeded * Mathf.Sin(angle * Mathf.Deg2Rad);
+            
+            car.Rb.linearVelocity = forwardVelocityMagnitude * car.transform.forward + (fixedVY ? initialVy_impulse : startingVelocityUp) * Vector3.up;
         }
     }
 }
