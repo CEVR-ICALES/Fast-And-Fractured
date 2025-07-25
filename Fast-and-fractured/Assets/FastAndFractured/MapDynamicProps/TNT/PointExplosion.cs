89c47a7e4b281446f70435a2a4c8f429832bec83
Adrián Ontiveros Cruz <adrionticruz@gmail.com>
2025-07-25 17:59:29 +0200

    general point explosion (to improve)

--- Fast-and-fractured/Assets/FastAndFractured/MapDynamicProps/TNT/PointExplosion.cs
+++ Fast-and-fractured/Assets/FastAndFractured/MapDynamicProps/TNT/PointExplosion.cs
@@ -1,16 +1,40 @@
+using System.Threading;
 using UnityEngine;
+using Utilities;
 
-public class PointExplosion : MonoBehaviour
+namespace FastAndFractured
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    public class PointExplosion : MonoBehaviour
     {
-        
+        // itll have radius, push force and center
+        [SerializeField] private float explosionRadius = 10f;
+        [SerializeField] private float pushForce = 1000000f;
+        [SerializeField] private Vector3 explosionCenterOffset = Vector3.zero;
+        [SerializeField] private ExplosionForce explosionHitbox;
+        [SerializeField] private float damagePercentage = 0.5f;
+        [SerializeField] private GameObject visuals;
+
+        private void Start()
+        {
+            explosionHitbox.ExplosionCollider = explosionHitbox.GetComponent<SphereCollider>();
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (collision.gameObject.TryGetComponent(out PhysicsBehaviour physicsBehaviour) && physicsBehaviour.StatsController != null)
+            {
+                if (explosionHitbox != null)
+                {
+                    explosionHitbox.ActivateExplosionHitbox(explosionRadius, pushForce, explosionCenterOffset);
+                    visuals.SetActive(false);
+                    TimerSystem.Instance.CreateTimer(0.5f, onTimerDecreaseComplete: () =>
+                    {
+                        physicsBehaviour.StatsController.TakeEndurance(physicsBehaviour.StatsController.MaxEndurance * damagePercentage, false, gameObject);
+                        gameObject.SetActive(false);
+                    });
+                }
+            }
+        }
     }
 }
