7ac7526f29305448df34081916655b8cd314b0de
Carlos Roig Garcia <carlos.roig.garcia9@gmail.com>
2025-10-24 23:11:22 +0200

    BrekeablePlatformsConstructionSite

--- Fast-and-fractured/Assets/FastAndFractured/MapDynamicProps/BreakablePlatforms/BreakablePlatform.cs
+++ Fast-and-fractured/Assets/FastAndFractured/MapDynamicProps/BreakablePlatforms/BreakablePlatform.cs
@@ -10,17 +10,36 @@ public class BreakablePlatform : MonoBehaviour
     [SerializeField] private float semiBreakTimePercentage = 0.5f;
     [SerializeField] private float bulletsSubtractTime = 0.2f;
     [SerializeField] private float explosionSubtractTime = 0.5f;
-    [SerializeField] private MeshCollider meshCollider;
-    [SerializeField] private MeshRenderer platform;
-    [SerializeField] private MeshRenderer semiBreakePlatform;
-    [SerializeField] private MeshRenderer breakePlatform;
+    private MeshCollider _meshCollider;
+    private MeshRenderer[] _platformFaces;
+    private int _platformIndex = 0;
+    [SerializeField]
+    private Transform SplinesParent;
     private int currentCharacters = 0;
     private ITimer breakTimeTimer = null;
     private ITimer semiBreakTimeTimer = null;
 
+    private const int BREAKABLE_PlATFORM_FASES = 3;
 
     private int bulletHits = 0;
     private bool isBreaking = false;
+
+    private void Start()
+    {
+        _platformFaces = new MeshRenderer[BREAKABLE_PlATFORM_FASES];
+        for(int i = 0; i < BREAKABLE_PlATFORM_FASES; ++i)
+        {
+            if (SplinesParent.GetChild(i) != null) 
+            {
+                _platformFaces[i] = SplinesParent.GetChild(i).GetComponent<MeshRenderer>();
+                if (i != 0 && _platformFaces[i].enabled)
+                {
+                    _platformFaces[i].enabled = false;
+                }
+            }
+        }
+        _meshCollider = _platformFaces[0].GetComponent<MeshCollider>();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -83,14 +102,15 @@ public class BreakablePlatform : MonoBehaviour
     private void BreakPlatform()
     {
         isBreaking = true;
-        semiBreakePlatform.enabled = false;
-        meshCollider.enabled = false;
-        breakePlatform.enabled = true;
+        _platformFaces[_platformIndex - 1].enabled = false;
+        _platformFaces[_platformIndex].enabled = true;
+        _meshCollider.enabled = false;
     }
 
     private void SemiBreakPlatform()
     {
-        platform.enabled = false;
-        semiBreakePlatform.enabled = true;
+        _platformIndex++;
+        _platformFaces[_platformIndex-1].enabled = false;
+        _platformFaces[_platformIndex].enabled = true;
     }
 }
