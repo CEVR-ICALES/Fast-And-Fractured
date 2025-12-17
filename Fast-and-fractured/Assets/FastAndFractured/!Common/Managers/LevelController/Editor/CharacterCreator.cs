2011f7fef84eb6c747fcac6bdc9ac932da3b8049
Carlos Roig Garcia <carlos.roig.garcia9@gmail.com>
2025-12-17 19:22:19 +0100

    CharacterCreatorLogicDone

--- Fast-and-fractured/Assets/FastAndFractured/!Common/Managers/LevelController/Editor/CharacterCreator.cs
+++ Fast-and-fractured/Assets/FastAndFractured/!Common/Managers/LevelController/Editor/CharacterCreator.cs
@@ -16,14 +16,22 @@ namespace FastAndFractured
         private string _characterName;
 
         private const string GAMEPLAY_CAR_NAME = "GameplayCar";
-        private const string PATH_TO_CHARACTERS = "FastAndFractured/Characters";
-        private const string PATH_TO_BASE_CAR_FROM_CHARACTERS = "/!Common/BaseCar.prefab";
+        private const string PATH_TO_CHARACTERS = "Assets/FastAndFractured/Characters";
+        private const string PATH_TO_BASE_CAR_FROM_CHARACTERS = "!Common/BaseCar.prefab";
+        private const string SCRIPTABLE_OBJECT_FOlDER = "ScriptableObjects";
+        private const string PREFABS_FOLDER = "Prefabs";
+        private const string CAR_DATA_SO_NAME = "CarData.asset";
+        private const string UNIQUE_ABILITY_SO_NAME = "UniqueAbilityData.asset";
+
         private const string CHARACTER_PATH = "Visuals/Character";
         private const string CHASSIS_PATH = "Visuals/Chassis";
         private const string FRONT_LEFT_WHEEL_PATH = "Visuals/WheelsVisuals/FrontLeftWheel";
         private const string FRONT_RIGHT_WHEEL_PATH = "Visuals/WheelsVisuals/FrontRightWheel";
         private const string BACK_LEFT_WHEEL_PATH = "Visuals/WheelsVisuals/BackLeftWheel";
         private const string BACK_RIGHT_WHEEL_PATH = "Visuals/WheelsVisuals/BackRightWheel";
+        private const string CHARACTER_MODEL_NAME = "Character";
+        private const string CHASIS_MODEL_NAME = "Chassis";
+        private const string WHEEL_MODEL_NAME = "WheelVisuals";
 
 
         [MenuItem("Tools/CharacterRelated/CharacterCreator", false, 50)]
@@ -43,44 +51,101 @@ namespace FastAndFractured
             _wheelModel = EditorGUILayout.ObjectField("Wheel Model", _wheelModel, typeof(GameObject), false) as GameObject;
             if (GUILayout.Button("Create", GUILayout.Height(35)))
             {
-                CreateNewCharacter();
+                if (CreateNewCharacter())
+                {
+                    Debug.Log("Character " +  _characterName + " Created Successfully");
+                }
+                else
+                {
+                    Debug.Log("")
+                }
             }
             EditorGUILayout.EndScrollView();
         }
 
-        void CreateNewCharacter()
+        bool CreateNewCharacter()
         {
-            if( _characterModel != null && _characterName != string.Empty && _chasisModel != null && _wheelModel != null)
+            if (_characterModel != null && _characterName != string.Empty && _chasisModel != null && _wheelModel != null)
             {
+                _characterModel.name = _characterName + CHARACTER_MODEL_NAME;
+                _characterModel.name = _characterName + CHASIS_MODEL_NAME;
+                _wheelModel.name = WHEEL_MODEL_NAME;
+
                 AssetDatabase.StartAssetEditing();
-                string relativePathToBaseCar = "Assets/" + PATH_TO_CHARACTERS + PATH_TO_BASE_CAR_FROM_CHARACTERS;
-                string baseCarPath = NormalizePath(relativePathToBaseCar);
-                Debug.Log(baseCarPath);
-                _baseCar = AssetDatabase.LoadAssetAtPath(baseCarPath, typeof(GameObject)) as GameObject;
-                string characterModelPath = NormalizePath(AssetDatabase.GetAssetPath(_characterModel));
-                string characterModelFolder = Path.GetDirectoryName(characterModelPath);
-                string parentFolder = Directory.GetParent(characterModelPath)?.FullName;
-                if (string.IsNullOrEmpty(parentFolder) || !parentFolder.StartsWith(Application.dataPath.Replace('/', '\\')))
+
+                string pathToBaseCar = Path.Combine(PATH_TO_CHARACTERS,PATH_TO_BASE_CAR_FROM_CHARACTERS);
+                string pathToNewCharacterParentFolder = Path.Combine(PATH_TO_CHARACTERS,_characterName);
+                if (!File.Exists(pathToBaseCar))
                 {
-                    return;
+                    Debug.LogError("File base car doesn't exit in " + pathToBaseCar);
+                    return false;
                 }
+                if (!Directory.Exists(pathToNewCharacterParentFolder))
+                {
+                    Directory.CreateDirectory(pathToNewCharacterParentFolder);
+                    Debug.Log("Folder " + pathToNewCharacterParentFolder + " created");
+                }
+
+                string prefabsDirectory = Path.Combine(pathToNewCharacterParentFolder, PREFABS_FOLDER);
+                string pathToCreateNewCharacter = Path.Combine(prefabsDirectory, _characterName + ".prefab");
+
+                if (!Directory.Exists(prefabsDirectory))
+                {
+                    Directory.CreateDirectory(prefabsDirectory);
+                    Debug.Log("Folder " + prefabsDirectory + " created");
+                }
+
+                _baseCar = AssetDatabase.LoadAssetAtPath(pathToBaseCar, typeof(GameObject)) as GameObject;
                 GameObject newBaseCar = PrefabUtility.InstantiatePrefab(_baseCar) as GameObject;
                 newBaseCar.name = _characterName + GAMEPLAY_CAR_NAME;
                 Transform characterHolder = newBaseCar.transform.Find(CHARACTER_PATH);
                 Transform chassisHolder = newBaseCar.transform.Find(CHASSIS_PATH);
                 Transform frontLeftWheelHolder = newBaseCar.transform.Find(FRONT_LEFT_WHEEL_PATH);
-                Transform frontRightWheelHolder = newBaseCar.transform.Find(FRONT_LEFT_WHEEL_PATH);
+                Transform frontRightWheelHolder = newBaseCar.transform.Find(FRONT_RIGHT_WHEEL_PATH);
                 Transform backLeftWheelHolder = newBaseCar.transform.Find(BACK_LEFT_WHEEL_PATH);
                 Transform backRightWheelHolder = newBaseCar.transform.Find(BACK_RIGHT_WHEEL_PATH);
-                (PrefabUtility.InstantiatePrefab(_characterModel) as GameObject).transform.parent = characterHolder;
-                (PrefabUtility.InstantiatePrefab(_chasisModel) as GameObject).transform.parent = chassisHolder;
-            }
-        }
+                PrefabUtility.InstantiatePrefab(_characterModel, characterHolder);
+                PrefabUtility.InstantiatePrefab(_chasisModel, chassisHolder);
+                PrefabUtility.InstantiatePrefab(_wheelModel, frontLeftWheelHolder);
+                PrefabUtility.InstantiatePrefab(_wheelModel, frontRightWheelHolder);
+                PrefabUtility.InstantiatePrefab(_wheelModel, backLeftWheelHolder);
+                PrefabUtility.InstantiatePrefab(_wheelModel, backRightWheelHolder);
+                PrefabUtility.SaveAsPrefabAsset(newBaseCar, pathToCreateNewCharacter);
 
-        public static string NormalizePath(string path)
-        {
-            if (string.IsNullOrEmpty(path)) return path;
-            return path.Replace('\\', '/');
+                string scriptableObjectDirectory = Path.Combine(pathToNewCharacterParentFolder, SCRIPTABLE_OBJECT_FOlDER);
+
+
+                if (!Directory.Exists(scriptableObjectDirectory))
+                {
+                    Directory.CreateDirectory(scriptableObjectDirectory);
+                    Debug.Log("Folder " + scriptableObjectDirectory + " created");
+                }
+                string carDataSOPath = Path.Combine(scriptableObjectDirectory,CAR_DATA_SO_NAME);
+
+                if(!File.Exists(scriptableObjectDirectory))
+                {
+                    CharacterData characterData = new CharacterData();
+                    characterData.name = _characterName + CAR_DATA_SO_NAME;
+                    characterData.CharacterName = _characterName;
+                    AssetDatabase.CreateAsset(characterData, carDataSOPath);
+                    newBaseCar.GetComponent<StatsController>().CharacterData = characterData;
+                    Debug.Log("File " +  carDataSOPath + " created");
+                }
+
+                string uniqueAbilitySOPath = Path.Combine(scriptableObjectDirectory, UNIQUE_ABILITY_SO_NAME);
+
+                if (!File.Exists(scriptableObjectDirectory))
+                {
+                    AbilityData abilityData = new AbilityData();
+                    abilityData.name = _characterName + UNIQUE_ABILITY_SO_NAME;
+                    AssetDatabase.CreateAsset(abilityData, uniqueAbilitySOPath);
+                    Debug.Log("File " + abilityData + " created");
+                }
+
+                AssetDatabase.StopAssetEditing();
+                return true;
+            }
+            return false;
         }
     }
 }
