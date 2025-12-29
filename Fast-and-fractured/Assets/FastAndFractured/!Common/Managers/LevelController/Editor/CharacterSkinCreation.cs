3e1fb8268bc632c9134dca1d1a7441fceebdbef0
CarlosRoig09 <134726851+CarlosRoig09@users.noreply.github.com>
2025-12-29 19:49:17 +0100

    LogicToSetCharacterSkinDone (Not tested)

--- Fast-and-fractured/Assets/FastAndFractured/!Common/Managers/LevelController/Editor/CharacterSkinCreation.cs
+++ Fast-and-fractured/Assets/FastAndFractured/!Common/Managers/LevelController/Editor/CharacterSkinCreation.cs
@@ -8,18 +8,15 @@ using Enums;
 
 public class CharacterSkinCreation : EditorWindow
 {
-    private class CharacterSkin
-    {
-        public Material[] CharacterMaterials = new Material[1];
-        public Material[] ChasisMaterials = new Material[1];
-        public Material[] WheelMaterials = new Material[1];
-    }
+   
 
     private Vector2 _scrollPosition;
     private string _characterName;
+    private string _currentCharacterName;
     private int _toalSkinCount;
+    private float _skinGroupValue = 0;
 
-    private CharacterSkin[] _characterSkin = new CharacterSkin[0];
+    private CharacterSkinsCountFileGenerator.CharacterSkin[] _characterSkin = new CharacterSkinsCountFileGenerator.CharacterSkin[0];
     [MenuItem("Tools/CharacterRelated/CharacterSkinCreation", false, 50)]
     public static void ShowWindow()
     {
@@ -38,11 +35,34 @@ public class CharacterSkinCreation : EditorWindow
         EditorGUILayoutArrays.LabelArrayField(arrayFieldSettings, charactersWithSkins);
 
         EditorGUILayout.LabelField("Character you want an skin for");
-        _characterName = EditorGUILayout.TextField("Character Name", _characterName);
-        if (_characterName != null)
+        _currentCharacterName = EditorGUILayout.TextField("Character Name", _currentCharacterName);
+        
+        if (GUILayout.Button("Select and continue", GUILayout.Height(35)))
         {
-            if (CharacterSkinsCountFileGenerator.CharacterFolderExist(_characterName))
+            _skinGroupValue = 0;
+            if (_currentCharacterName == null)
             {
+                EditorGUILayout.LabelField("No name set on the text field.");
+            }
+            else if (!CharacterSkinsCountFileGenerator.CharacterFolderExist(_currentCharacterName))
+            {
+                EditorGUILayout.LabelField("Character folder doesn't exist. Pls use the character creation tool or" +
+                   "create the folder on Assets/FastAndFractured/Resources/CharacterSkins");
+            }
+            else
+            {
+                _skinGroupValue = 1;
+                if (_characterName != _currentCharacterName)
+                {
+                    _characterSkin = Array.Empty<CharacterSkinsCountFileGenerator.CharacterSkin>();
+                    _characterName = _currentCharacterName;
+                }
+            }
+        }
+
+        EditorGUILayout.BeginFadeGroup(_skinGroupValue);
+        if (_skinGroupValue==1)
+        {
                 _toalSkinCount = CharacterSkinsCountFileGenerator.ReturnSkinCountOfACharacter(_characterName);
                 int characterSkinSize = _characterSkin.Length;
                 characterSkinSize = EditorGUILayout.IntField("Size", characterSkinSize);
@@ -50,15 +70,15 @@ public class CharacterSkinCreation : EditorWindow
                 _characterSkin = ResizeArray(_characterSkin, characterSkinSize);
                 if (_characterSkin[0] == null)
                 {
-                    for(int i = 0; i < _toalSkinCount; i++)
+                    for (int i = 0; i < _toalSkinCount; i++)
                     {
-                        _characterSkin[i] = CreateACharacterSkinFromAlreadyExistingSkin(_characterSkin[i],_characterName,i);
+                        _characterSkin[i] = CreateACharacterSkinFromAlreadyExistingSkin(_characterSkin[i], _characterName, i);
                     }
                 }
                 for (int i = 0; i < characterSkinSize; i++)
                 {
                     bool open = true;
-                    open = EditorGUILayout.Foldout(open, "Skin num " + i);
+                    open = EditorGUILayout.Foldout(open, "Skin num _" + i + 1);
                     if (open)
                     {
                         _characterSkin[i] = CharacterSkinLabels(_characterSkin[i]);
@@ -66,39 +86,34 @@ public class CharacterSkinCreation : EditorWindow
                 }
                 if (GUILayout.Button("Create", GUILayout.Height(35)))
                 {
+                   
                 }
+
             }
-            else
-            {
-                EditorGUILayout.LabelField("Character folder doesn't exist. Pls use the character creation tool or" +
-                    "create the folder on Assets/FastAndFractured/Resources/CharacterSkins");
-            }
-        }
-        else
-        {
-            EditorGUILayout.LabelField("No name set on the text field.");
-        }
-            EditorGUILayout.EndScrollView();
+            
+        EditorGUILayout.EndFadeGroup();
+
+        EditorGUILayout.EndScrollView();
     }
 
-    private CharacterSkin CharacterSkinLabels(CharacterSkin characterSkin)
+    private CharacterSkinsCountFileGenerator.CharacterSkin CharacterSkinLabels(CharacterSkinsCountFileGenerator.CharacterSkin characterSkin)
     {
         if (characterSkin==null)
         {
-            characterSkin = new CharacterSkin();
+            characterSkin = new CharacterSkinsCountFileGenerator.CharacterSkin();
         }
         EditorGUILayoutArrays.ArrayFieldSettings characterMaterialSettings = new EditorGUILayoutArrays.ArrayFieldSettings("Character Materials [in order]");
         characterSkin.CharacterMaterials = EditorGUILayoutArrays.MaterialArrayField(characterMaterialSettings, characterSkin.CharacterMaterials);
         EditorGUILayoutArrays.ArrayFieldSettings chassisMaterialSettings = new EditorGUILayoutArrays.ArrayFieldSettings("Chasis Materials [in order]");
         characterSkin.ChasisMaterials = EditorGUILayoutArrays.MaterialArrayField(chassisMaterialSettings, characterSkin.ChasisMaterials);
         EditorGUILayoutArrays.ArrayFieldSettings wheelMaterialSettings = new EditorGUILayoutArrays.ArrayFieldSettings("Wheel Materials [in order]");
-        characterSkin.ChasisMaterials = EditorGUILayoutArrays.MaterialArrayField(wheelMaterialSettings, characterSkin.WheelMaterials);
+        characterSkin.WheelMaterials = EditorGUILayoutArrays.MaterialArrayField(wheelMaterialSettings, characterSkin.WheelMaterials);
         return characterSkin;
     }
 
-    private CharacterSkin CreateACharacterSkinFromAlreadyExistingSkin(CharacterSkin characterSkin,string name,int skinCount)
+    private CharacterSkinsCountFileGenerator.CharacterSkin CreateACharacterSkinFromAlreadyExistingSkin(CharacterSkinsCountFileGenerator.CharacterSkin characterSkin,string name,int skinCount)
     {
-        characterSkin = new CharacterSkin();
+        characterSkin = new CharacterSkinsCountFileGenerator.CharacterSkin();
         characterSkin.CharacterMaterials = CharacterSkinsCountFileGenerator.ReturnMaterialsOfCharacterPrefabPart(name,skinCount,CharacterPrefabParts.Character);
         characterSkin.ChasisMaterials = CharacterSkinsCountFileGenerator.ReturnMaterialsOfCharacterPrefabPart(name, skinCount, CharacterPrefabParts.Chassis);
         characterSkin.WheelMaterials = CharacterSkinsCountFileGenerator.ReturnMaterialsOfCharacterPrefabPart(name, skinCount, CharacterPrefabParts.Wheel);
--- Fast-and-fractured/Assets/FastAndFractured/!Common/Managers/LevelController/Editor/CharacterSkinsCountFileGenerator.cs
+++ Fast-and-fractured/Assets/FastAndFractured/!Common/Managers/LevelController/Editor/CharacterSkinsCountFileGenerator.cs
@@ -11,11 +11,19 @@ namespace FastAndFractured {
         private const string RESOURCES_FOLDER_PATH = "Assets/FastAndFractured/Resources";
         private const string LIST_OF_CHARACTER_SKINS = "/ListOfCharacterSkins.asset";
         private const string SKIN_PREFIX = "_";
+        private const int SKIN_STARTING_INT = 1;
         private const string SKIN_CHARACTER_PREFAB_FOLDER = "Character";
         private const string SKIN_CHASSIS_PREFAB_FOLDER = "Chassis";
         private const string SKIN_WHEELS_PREFAB_FOLDER = "Wheels";
 
         private static string characterFolderPath = RESOURCES_FOLDER_PATH + "/" + LevelConstants.SKINS_LOADER_PATH;
+
+        public class CharacterSkin
+        {
+            public Material[] CharacterMaterials = new Material[1];
+            public Material[] ChasisMaterials = new Material[1];
+            public Material[] WheelMaterials = new Material[1];
+        }
 
         [MenuItem("Tools/GenerateCharacterSkinCountFile")]
         public static void GenerateCharacterSkinsCountFile()
@@ -42,6 +50,7 @@ namespace FastAndFractured {
 
             AssetDatabase.CreateAsset(asset, characterFolderPath + LIST_OF_CHARACTER_SKINS);
             AssetDatabase.SaveAssets();
+            AssetDatabase.Refresh();
         }
 
         public static void CreateNewCharacterInCharacterSkinsFolder(string CharacterName)
@@ -92,16 +101,95 @@ namespace FastAndFractured {
             switch (characterPrefabParts)
             {
                 case CharacterPrefabParts.Character:
-                    return ReturnAllMaterialsFromFolder(Path.Combine(characterSkinPath,SKIN_CHARACTER_PREFAB_FOLDER));
+                    return GetAllMaterialsFromFolder(Path.Combine(characterSkinPath,SKIN_CHARACTER_PREFAB_FOLDER));
                 case CharacterPrefabParts.Chassis:
-                    return ReturnAllMaterialsFromFolder(Path.Combine(characterSkinPath, SKIN_CHASSIS_PREFAB_FOLDER));
+                    return GetAllMaterialsFromFolder(Path.Combine(characterSkinPath, SKIN_CHASSIS_PREFAB_FOLDER));
                 case CharacterPrefabParts.Wheel:
-                    return ReturnAllMaterialsFromFolder(Path.Combine(characterSkinPath, SKIN_WHEELS_PREFAB_FOLDER));
+                    return GetAllMaterialsFromFolder(Path.Combine(characterSkinPath, SKIN_WHEELS_PREFAB_FOLDER));
             }
             return default;
         }
 
-        private static Material[] ReturnAllMaterialsFromFolder(string folderPath)
+        public static void SaveNewSkinsInCharacterSkinDirectory(CharacterSkin[] characterSkins, string name)
+        {
+            string characterPath = Path.Combine(LevelConstants.SKINS_LOADER_PATH, name);
+            if (!DirectoryExist(characterPath))
+            {
+                return;
+            }
+            for(int i = 0; i < characterSkins.Length; i++)
+            {
+                string skinFolder = Path.Combine(characterFolderPath,SKIN_PREFIX + (i + SKIN_STARTING_INT));
+                string characterSkinFolder = NormalizePath(Path.Combine(skinFolder,SKIN_CHARACTER_PREFAB_FOLDER));
+                string chassisSkinFolder = NormalizePath(Path.Combine(skinFolder, SKIN_CHASSIS_PREFAB_FOLDER));
+                string wheelSkinFolder = NormalizePath(Path.Combine(skinFolder, SKIN_WHEELS_PREFAB_FOLDER));
+                SetAllMaterialsFromFolder(characterSkinFolder, characterSkins[i].CharacterMaterials);
+                SetAllMaterialsFromFolder(chassisSkinFolder, characterSkins[i].ChasisMaterials);
+                SetAllMaterialsFromFolder(wheelSkinFolder, characterSkins[i].WheelMaterials);
+            }
+        }
+
+        private static void SetAllMaterialsFromFolder(string folderPath,Material[] newMaterials)
+        {
+            Material[] materialsFromFolder = GetAllMaterialsFromFolder(folderPath);
+            List<int> materialsIndexToChange = new List<int>();
+            for(int i = 0; i < materialsFromFolder.Length && i<newMaterials.Length; i++)
+            {
+                if (materialsFromFolder[i] != newMaterials[i])
+                {
+                    materialsIndexToChange.Add(i);
+                }
+            }
+            if (materialsIndexToChange.Count > 0)
+            {
+                string[] assetsPath = new string[materialsIndexToChange.Count];
+                Material[] adbNewMaterials = newMaterials;
+                newMaterials = new Material[materialsIndexToChange.Count];
+                for (int i = 0; i < materialsIndexToChange.Count; i++)
+                {
+                    Material material = materialsFromFolder[materialsIndexToChange[i]];
+                    assetsPath[i] = NormalizePath(Path.Combine(folderPath, material.name + ".mat"));
+                    newMaterials[i] = adbNewMaterials[materialsIndexToChange[i]];
+                }
+                bool deleted = DeleteAsset(assetsPath);
+            }
+            SaveAssets(newMaterials, folderPath, ".mat");
+        }
+
+        private static bool DeleteAsset(string assetPath)
+        {
+            bool deleted = AssetDatabase.DeleteAsset(assetPath);
+            AssetDatabase.SaveAssets();
+            AssetDatabase.Refresh();
+            return deleted;
+        }
+        private static bool DeleteAsset(string[] assetsPath)
+        {
+            List<string> outFailedPaths = new List<string>();
+            bool deleted = AssetDatabase.DeleteAssets(assetsPath,outFailedPaths);
+            foreach(string outFailedPath in outFailedPaths)
+            {
+                Debug.Log("File " + outFailedPath + " couldn't been deleted due to path not existing.");
+            }
+            AssetDatabase.SaveAssets();
+            AssetDatabase.Refresh();
+            return deleted;
+        }
+
+        private static void SaveAssets<T>(T[] objectsUnity,string folderPath,string extension) where T : Object
+        {
+            foreach (T objectUnity in objectsUnity)
+            {
+
+                string assetPath = Path.Combine(folderPath, objectUnity.name + extension);
+                string normalizedAssetPath = NormalizePath(assetPath);
+                AssetDatabase.CreateAsset(objectUnity,normalizedAssetPath);
+            }
+            AssetDatabase.SaveAssets();
+            AssetDatabase.Refresh();
+        }
+
+        private static Material[] GetAllMaterialsFromFolder(string folderPath)
         {
             string normalizedFolderPath = NormalizePath(folderPath);
            return Resources.LoadAll<Material>(normalizedFolderPath);
@@ -116,6 +204,7 @@ namespace FastAndFractured {
             }
             return true;
         }
+
 
         public static string NormalizePath(string path)
         {
