b83ff5ecb4568986630cfd540610c0e52bab8530
Carlos Roig Garcia <carlos.roig.garcia9@gmail.com>
2026-09-24 13:09:59 +0200

    OnCollisionShakeDifferenceBetweenListenerAndSource

--- Fast-and-fractured/Assets/FastAndFractured/CameraBehaviours/Scripts/ScreenShakeOnCollisionHandle.cs
+++ Fast-and-fractured/Assets/FastAndFractured/CameraBehaviours/Scripts/ScreenShakeOnCollisionHandle.cs
@@ -3,8 +3,16 @@ using StateMachine;
 using Unity.Cinemachine;
 using UnityEngine;
 
+public enum ScreenShakeOnCollisionType
+{
+    Listener,
+    Source,
+}
 public class ScreenShakeOnCollisionHandle : MonoBehaviour
 {
+    [SerializeField]
+    private ScreenShakeOnCollisionType type = ScreenShakeOnCollisionType.Source;
+    
     [SerializeField]
     private Collider shakeCollider;
 
@@ -23,38 +31,97 @@ public class ScreenShakeOnCollisionHandle : MonoBehaviour
 
     private float _boundToCenter;
 
-    private const float MAX_PERCENTAGE = 100f;
+    [SerializeField]
+    private bool itDependOnSpeed = false;
+
+    [SerializeField]
+    private float referenceHighSpeed = 300f;
+    [SerializeField]
+    [Range(10,100)]
+    private float strenghtOfFactorSpeed = 80f;
+
+    [SerializeField]
+    private Rigidbody ownRigydbody;
 
     [SerializeField]
     private ScreenShakeSourceController screenShakeSourceController;
+
+    [Tooltip("This value is only needed if you're the listener.")]
+    [SerializeField]
+    private CameraBehaviours ownCameraBehaviour;
+
+    private const float MAX_PERCENTAGE = 100f;
+
+    private const float SPEED_TO_METER_PER_SECOND = 3.6f;
+
+    
     private void Start()
     {
         if (shakeCollider == null)
         {
             shakeCollider = GetComponent<Collider>();
         }
+        if (screenShakeSourceController == null)
+        {
+            screenShakeSourceController = GetComponent<ScreenShakeSourceController>();
+        }
+        if (ownCameraBehaviour == null&&type==ScreenShakeOnCollisionType.Listener)
+        {
+            if((ownCameraBehaviour = transform.parent.parent.GetComponentInChildren<CameraBehaviours>())==null)
+            shakeCollider.enabled=false;
+        }
         _boundToCenter = shakeCollider.bounds.max.magnitude;
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("He entrado y soy " + other.gameObject);
+        CameraBehaviours cameraBehaviours;
+        if(type== ScreenShakeOnCollisionType.Source)
+        {
         Transform carBaseReference = other.transform.parent;
-        CameraBehaviours otherCameraBehaviours = carBaseReference != null ? carBaseReference.GetComponentInChildren<CameraBehaviours>() : null;
-        if(otherCameraBehaviours!=null)
+         cameraBehaviours = carBaseReference != null ? carBaseReference.GetComponentInChildren<CameraBehaviours>() : null;
+        }
+        else
+        {
+            cameraBehaviours = ownCameraBehaviour;
+        }
+
+        HandleSourceCollision(cameraBehaviours,other);
+    }
+
+    private void HandleSourceCollision(CameraBehaviours cameraBehaviours,Collider other)
+    {
+        
+        if(cameraBehaviours!=null)
         {
             float baseImpactForce = screenShakeProfile.impactForce;
-            float baseImpactTime = screenShakeProfile.impactTime;
             if(itDependOnDistance)
             {
                 float distanceToCenter = (other.transform.position - shakeCollider.bounds.center).magnitude;
                 float distanceFactor = (1 + ((maxDistanceDifference - distanceToCenter )/distanceToCenter))*(strenghtOfFactorDistance/MAX_PERCENTAGE);
                 screenShakeProfile.impactForce*=distanceFactor;
-                Debug.Log("BoundToCenter : " + _boundToCenter + " DistanceToCenter: " + distanceToCenter + " distanceFactor: " + distanceFactor);
             }
-            screenShakeSourceController.PlayLocalShakeFromProfile(otherCameraBehaviours,screenShakeProfile);
+            if (itDependOnSpeed)
+            {
+                if (ownRigydbody == null)
+                {
+                    Debug.LogWarning("The variable ownRigydbody form " + this + " is null. Assign the corresponding rigydbody or untrigger the itDependOnSpeedFlag.");
+                    return;
+                }
+                float combinedSpeed = other.attachedRigidbody!=null ? ownRigydbody.linearVelocity.magnitude + other.attachedRigidbody.linearVelocity.magnitude : ownRigydbody.linearVelocity.magnitude;
+                float referenceHighSpeedInUnitsPerSecond = referenceHighSpeed/SPEED_TO_METER_PER_SECOND;
+                float speedFactor = combinedSpeed/referenceHighSpeedInUnitsPerSecond;
+                float speedFactorWithPercentageApplied = speedFactor * (strenghtOfFactorSpeed/MAX_PERCENTAGE);
+                screenShakeProfile.impactForce*=speedFactorWithPercentageApplied;
+            }
+            screenShakeSourceController.PlayLocalShakeFromProfile(cameraBehaviours,screenShakeProfile);
             screenShakeProfile.impactForce = baseImpactForce;
         }
+    }
+
+    private void HandleListenerCollision(Collider other)
+    {
+        
     }
 }
     
